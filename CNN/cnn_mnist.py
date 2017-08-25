42ee502f3f213f04dbceddd715308e435b7bfb16
fengjiran <fengjiran@foxmail.com>
2017-08-25 14:50:15 +0800

    add train phase

--- CNN/cnn_mnist.py
+++ CNN/cnn_mnist.py
@@ -26,6 +26,16 @@ img_shape = (img_size, img_size)
 num_classes = 10
 num_channels = 1
 
+filter_size1 = 5
+num_filters1 = 16
+
+filter_size2 = 5
+num_filters2 = 36
+
+fc_size = 128
+
+batch_size = 64
+
 
 def plot_images(images, cls_true, cls_pred=None, img_shape=(28, 28)):
     """Plot 9 images in a 3x3 grid.
@@ -116,3 +126,63 @@ def new_fc_layer(inpt,
         layer = tf.nn.relu(layer)
 
     return layer
+
+
+x = tf.placeholder(tf.float32, [None, img_size_flat], name='x')
+x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
+
+y_true = tf.placeholder(tf.float32, [None, 10], name='y_true')
+y_true_cls = tf.argmax(y_true, axis=1)
+
+
+layer_conv1, weights_conv1 = new_conv_layer(inpt=x_image,
+                                            num_input_channels=num_channels,
+                                            filter_size=filter_size1,
+                                            num_filters=num_filters1,
+                                            use_pooling=True)
+
+layer_conv2, weights_conv2 = new_conv_layer(inpt=layer_conv1,
+                                            num_input_channels=num_filters1,
+                                            filter_size=filter_size2,
+                                            num_filters=num_filters2,
+                                            use_pooling=True)
+
+layer_flat, num_features = flatten_layer(layer_conv2)
+
+layer_fc1 = new_fc_layer(inpt=layer_flat,
+                         num_inputs=num_features,
+                         num_outputs=fc_size,
+                         use_relu=True)
+
+layer_fc2 = new_fc_layer(inpt=layer_fc1,
+                         num_inputs=fc_size,
+                         num_outputs=num_classes,
+                         use_relu=False)
+
+y_pred = tf.nn.softmax(layer_fc2)
+y_pred_cls = tf.argmax(y_pred, axis=1)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
+                                                              labels=y_true))
+
+optimizer = tf.train.AdamOptimizer(1e-4).minimize(cost)
+
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for epoch in range(100):
+        print('Epoch: {0}/{1}'.format(epoch, 100))
+
+        for iters in range(int(len(data.train.labels) / batch_size)):
+            # print('Iters: {0}/{1}'.format(iters, int(len(data.train.labels) / batch_size)))
+
+            x_batch, y_true_batch = data.train.next_batch(batch_size)
+            sess.run(optimizer, feed_dict={x: x_batch,
+                                           y_true: y_true_batch})
+
+        acc = accuracy.eval(feed_dict={x: data.test.images,
+                                       y_true: data.test.labels})
+
+        print('Accuracy {}'.format(acc))
