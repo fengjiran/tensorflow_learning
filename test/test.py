34d6ea329bee08bcf35b11926f7a4fad59fc6326
fengjiran <fengjiran@foxmail.com>
2017-09-19 00:03:21 +0800

    add test

--- test/test.py
+++ test/test.py
@@ -1,16 +1,22 @@
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 
-x = tf.Variable(1.0)
-x_plus_1 = tf.assign_add(x, 1)
+g = tf.get_default_graph()
+
+x = tf.Variable(1.0, name='x')
+# x_plus_1 = tf.assign_add(x, 1)
+# y = x
 # y = tf.identity(x)
+# y = x_plus_1
 
-with tf.control_dependencies([x_plus_1]):
-    print('hello')
-    # y = x_plus_1
-    # y = tf.identity(x)
-    # z = tf.identity(x, name='x')
-
+with tf.control_dependencies([tf.assign_add(x, 1)]):
+    y = x
+# y = tf.identity(x)
+# y = x_plus_1
+# y = tf.identity(x)
+# z = tf.identity(x, name='x')
+# print(g.get_operations())
+print(tf.global_variables())
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
@@ -18,7 +24,7 @@ with tf.Session() as sess:
     for i in range(5):
         # print(sess.run(x), sess.run(y), sess.run(x_plus_1))
         # print(sess.run(x_plus_1), sess.run(x), sess.run(y))
-        print(sess.run(x_plus_1))
+        print(sess.run(tf.assign_add(x, 1)), sess.run(y))
 
 
 # g1 = tf.get_default_graph()
