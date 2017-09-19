c4d180c4dd7a8942c67777ecb5e673235b5889b7
fengjiran <fengjiran@foxmail.com>
2017-09-19 20:57:24 +0800

    add model

---
+++ Inpainting/model.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+import tensorflow as tf
+
+
+def context_encoder():
+    pass
---
+++ Inpainting/train_imagenet_100k.py
@@ -0,0 +1,15 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+import platform
+import pickle
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+from utils import load_image
+from utils import crop_random
+
+
