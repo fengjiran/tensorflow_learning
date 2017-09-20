dbbf351c4e9e3840b5d228f758c04b91401bd900
fengjiran <fengjiran@foxmail.com>
2017-09-20 14:09:55 +0800

    add platform check

--- Inpainting/train_imagenet_100k.py
+++ Inpainting/train_imagenet_100k.py
@@ -13,3 +13,15 @@ from utils import load_image
 from utils import crop_random
 
 
+if platform.system() == 'Windows':
+    trainset_path = 'X:\\DeepLearning\\imagenet_trainset.pickle'
+    testset_path = 'X:\\DeepLearning\\imagenet_testset.pickle'
+    dataset_path = 'X:\\DeepLearning\\ImageNet_100K'
+    result_path = 'E:\\Scholar_Project\\Inpainting\\Context_Encoders\\imagenet'
+    model_path = 'E:\\Scholar_Project\\Inpainting\\Context_Encoders\\models\\imagenet'
+elif platform.system() == 'Linux':
+    trainset_path = '/home/richard/Deep_Learning_Projects/Inpainting/Context_Encoders/imagenet_trainset.pickle'
+    testset_path = '/home/richard/Deep_Learning_Projects/Inpainting/Context_Encoders/imagenet_testset.pickle'
+    dataset_path = '/home/richard/datasets/ImageNet_100K'
+    result_path = '/home/richard/Deep_Learning_Projects/Inpainting/Context_Encoders/imagenet'
+    model_path = '/home/richard/Deep_Learning_Projects/Inpainting/Context_Encoders/models/imagenet'
