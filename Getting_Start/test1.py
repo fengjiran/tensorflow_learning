f5d7c2f21dd888dba658db598c366ce72efeea59
fengjiran <fengjiran@foxmail.com>
2017-09-25 22:18:14 +0800

    test

--- Getting_Start/test1.py
+++ Getting_Start/test1.py
@@ -21,16 +21,28 @@ b = tf.Variable(tf.zeros([1, 3]))
 
 y = tf.matmul(x_data**2, W2) + tf.matmul(x_data, W1) + b
 
-loss = tf.reduce_mean(tf.sqrt(tf.square(y - y_data)))
+loss = tf.sqrt(tf.reduce_mean(tf.square(y - y_data)))
 optimizer = tf.train.GradientDescentOptimizer(0.005)
-train = optimizer.minimize(loss)
+
+grads = optimizer.compute_gradients(loss, [W1, W2, b])
+# grads_vars_D = map(lambda gv: [tf.clip_by_value(gv[0], -10., 10.), gv[1]], grads_vars_D)
+grads = [(tf.clip_by_value(gv[0], -1., 1.), gv[1]) for gv in grads]
+# grads = map(lambda gv: [tf.clip_by_value(gv[0], -1., 1.), gv[1]], grads)
+train_op = optimizer.apply_gradients(grads)
+
+# train = optimizer.minimize(loss)
+saver = tf.train.Saver()
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
+    # sa = tf.train.import_meta_graph('E:\\TensorFlow_Learning\\Getting_Start\\my_model\\model.meta')
+    saver.restore(sess, 'E:\\TensorFlow_Learning\\Getting_Start\\my_model\\model')
 
     for step in range(6000):
-        sess.run(train)
+        sess.run(train_op)
         if step % 20 == 0:
             print(sess.run(loss))
+
+    saver.save(sess, 'E:\\TensorFlow_Learning\\Getting_Start\\my_model\\model')
