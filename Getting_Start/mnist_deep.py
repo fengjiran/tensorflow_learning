22d8a65d89d0b5dfdbd09cb99fe927d388f6932d
fengjiran <fengjiran@foxmail.com>
2017-10-10 14:50:30 +0800

    test for 1050ti

--- Getting_Start/mnist_deep.py
+++ Getting_Start/mnist_deep.py
@@ -182,8 +182,9 @@ def main(_):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str,
-                        default='/home/richard/datasets/mnist',
-                        # default='E:\\deeplearning_experiments\\datasets\\mnist',
+                        # default='/home/richard/datasets/mnist',
+                        default='E:\\deeplearning_experiments\\datasets\\mnist',
+                        # default='E:\\fashion_mnist',
                         help='Directory for storing input data')
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
