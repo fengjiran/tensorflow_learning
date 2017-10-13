18edcd941d6cce644ca10638203d41998f62166b
fengjiran <fengjiran@foxmail.com>
2017-10-13 15:30:23 +0800

    add train paris module

---
+++ Inpainting/train_pairs_dcgan.py
@@ -0,0 +1,11 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+from glob import glob
+import platform
+import pickle
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
