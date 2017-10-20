4e028d10ec513dad6e6f20efd25fc2faf609024b
fengjiran <fengjiran@foxmail.com>
2017-10-20 14:33:28 +0800

    change rules

--- Inpainting/context_encoder/train_imagenet_100k_dcgan_original.py
+++ Inpainting/context_encoder/train_imagenet_100k_dcgan_original.py
@@ -26,9 +26,8 @@ learning_rate_val = 0.0003
 weight_decay_rate = 0.00001
 momentum = 0.9
 batch_size = 128
-lambda_recon = 0.9
+lambda_recon = 0.99
 lambda_adv = 1 - lambda_recon
-alpha = 0.84
 
 overlap_size = 7
 hiding_size = 64
@@ -37,16 +36,16 @@ if platform.system() == 'Windows':
     trainset_path = 'X:\\DeepLearning\\imagenet_trainset.pickle'
     testset_path = 'X:\\DeepLearning\\imagenet_testset.pickle'
     dataset_path = 'X:\\DeepLearning\\ImageNet_100K'
-    result_path = 'E:\\TensorFlow_Learning\\Inpainting\\imagenet'
-    model_path = 'E:\\TensorFlow_Learning\\Inpainting\\models'
-    params = 'E:\\TensorFlow_Learning\\Inpainting\\params'
+    result_path = 'E:\\TensorFlow_Learning\\Inpainting\\context_encoder\\imagenet_100k\\images'
+    model_path = 'E:\\TensorFlow_Learning\\Inpainting\\context_encoder\\imagenet_100k\\models'
+    params_path = 'E:\\TensorFlow_Learning\\Inpainting\\context_encoder\\imagenet_100k\\params'
 elif platform.system() == 'Linux':
     trainset_path = '/home/richard/Deep_Learning_Projects/Inpainting/Context_Encoders/imagenet_trainset.pickle'
     testset_path = '/home/richard/Deep_Learning_Projects/Inpainting/Context_Encoders/imagenet_testset.pickle'
     dataset_path = '/home/richard/datasets/ImageNet_100K'
-    result_path = '/home/richard/TensorFlow_Learning/Inpainting/imagenet'
-    model_path = '/home/richard/TensorFlow_Learning/Inpainting/models'
-    params_path = '/home/richard/TensorFlow_Learning/Inpainting/params'
+    result_path = '/home/richard/TensorFlow_Learning/Inpainting/context_encoder/imagenet_100k/images'
+    model_path = '/home/richard/TensorFlow_Learning/Inpainting/context_encoder/imagenet_100k/models'
+    params_path = '/home/richard/TensorFlow_Learning/Inpainting/context_encoder/imagenet_100k/params'
 
 
 if not os.path.exists(trainset_path) or not os.path.exists(testset_path):
@@ -113,7 +112,7 @@ loss_D = loss_adv_D * lambda_adv
 # w_D = filter(lambda x: x.name.endswith('w:0'), var_D)
 
 var_G = tf.get_collection('gen_params_conv') + tf.get_collection('gen_params_bn')
-var_D = tf.get_collection('dis_params_conv')  # + tf.get_collection('dis_params_bn')
+var_D = tf.get_collection('dis_params_conv') + tf.get_collection('dis_params_bn')
 # loss_G = loss_G + weight_decay_rate * tf.reduce_mean(tf.stack(list(map(tf.nn.l2_loss, w_G))))
 # loss_D = loss_D + weight_decay_rate * tf.reduce_mean(tf.stack(list(map(tf.nn.l2_loss, w_D))))
 
@@ -157,7 +156,7 @@ with tf.Session() as sess:
     if not isFirstTimeTrain:
         saver.restore(sess, os.path.join(model_path, 'context_encoder_imagenet_model'))
 
-        with open(os.path.join(params_path, 'iters')) as f:
+        with open(os.path.join(params_path, 'iters'), 'rb') as f:
             iters = pickle.load(f)
     else:
         iters = 0
@@ -177,11 +176,13 @@ with tf.Session() as sess:
             index = int(start / batch_size)
             image_paths = trainset[start:end]['image_path'].values
             images_ori = map(load_image, image_paths)
+            images_ori = list(images_ori)
             is_none = np.sum([x is None for x in images_ori])
             if is_none > 0:
                 continue
 
             images_crops = map(crop_random, images_ori)
+            images_crops = list(images_crops)
             train_images, train_crops, _, _ = zip(*images_crops)
 
             train_images = np.array(train_images)  # images with holes,the inputs of context encoder
@@ -191,6 +192,7 @@ with tf.Session() as sess:
             if iters % 100 == 0:
                 test_image_paths = testset[:batch_size]['image_path'].values
                 test_image_ori = map(load_image, test_image_paths)
+                test_image_ori = list(test_image_ori)
 
                 test_images_crop = [crop_random(image_ori, x=32, y=32) for image_ori in test_image_ori]
                 test_images, test_crops, xs, ys = zip(*test_images_crop)
