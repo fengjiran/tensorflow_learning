0d65d8a0cfcbcf022ec1abe10c25d758137d1b6f
fengjiran <fengjiran@foxmail.com>
2017-11-17 09:02:16 +0800

    test code

--- Inpainting/globally_locally/train_imagenet.py
+++ Inpainting/globally_locally/train_imagenet.py
@@ -5,9 +5,9 @@ import platform
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-from utils import load_image
-from utils import crop_image_with_hole
+from utils import read_batch
 from utils import array_to_image
 
 from models import completion_network
@@ -35,50 +35,90 @@ iters_total = 120000  # total iters
 
 # placeholder
 is_training = tf.placeholder(tf.bool)
-global_step = tf.placeholder(tf.int64)
+# global_step = tf.placeholder(tf.int64)
 
 images = tf.placeholder(tf.float32, [batch_size, input_height, input_width, 3], name='images')
 images_with_hole = tf.placeholder(tf.float32, [batch_size, input_height, input_width, 3], name='images_with_holes')
-# holes = tf.placeholder(tf.float32, [batch_size, None, None, 3], name='holes')
 
-masks = tf.placeholder(tf.float32, [batch_size, input_height, input_width, 3], name='masks')
-ground_truth = tf.placeholder(tf.float32, [batch_size, gt_height, gt_width, 3], name='ground_truth')
+masks_c = tf.placeholder(tf.float32, [batch_size, input_height, input_width, 3], name='masks_c')
+# ground_truth = tf.placeholder(tf.float32, [batch_size, gt_height, gt_width, 3], name='ground_truth')
 
-# hole_height = tf.placeholder(tf.int32)
-# hole_width = tf.placeholder(tf.int32)
-# y_init = tf.placeholder(tf.int32)
-# x_init = tf.placeholder(tf.int32)
+x_locs = tf.placeholder(tf.int32, [batch_size])
+y_locs = tf.placeholder(tf.int32, [batch_size])
 
-local_mask_location = tf.placeholder(tf.int32, [batch_size, 2])
-# x_loc = tf.placeholder(tf.int32, [batch_size])
-# y_loc = tf.placeholder(tf.int32, [batch_size])
-
-# # mask for every image
-# mask = tf.pad(tensor=tf.ones([hole_height, hole_width]),
-#               paddings=[[input_height - hole_height - y_init, y_init], [x_init, input_width - hole_width - x_init]])
-# mask = tf.reshape(mask, [input_height, input_width, 1])
-# mask = tf.concat([mask] * 3, 2)
 labels_G = tf.ones([batch_size])
 labels_D = tf.concat([tf.ones([batch_size]), tf.zeros([batch_size])], axis=0)
 
 completed_images = completion_network(images_with_hole, is_training)
 
 # global discriminator inputs
-global_dis_inputs_fake = completed_images * masks + images_with_hole * (1 - masks)
+global_dis_inputs_fake = completed_images * masks_c + images_with_hole * (1 - masks_c)
 # the global_dis_inputs_real = images
 
 # local discriminator inputs
-# local_dis_inputs_fake = global_dis_inputs_fake[:, y_loc:y_loc + 128, x_loc:x_loc + 128, :]
+local_dis_inputs_fake = tf.map_fn(fn=lambda args: tf.image.crop_to_bounding_box(args[0], args[1], args[2], gt_height, gt_width),
+                                  elems=(global_dis_inputs_fake, y_locs, x_locs),
+                                  dtype=tf.float32)
+# crop sample from real samples of 128*128
+offset_x = np.random.randint(0, input_width - gt_width)
+offset_y = np.random.randint(0, input_height - gt_height)
+local_dis_inputs_real = tf.image.crop_to_bounding_box(images, offset_y, offset_x, gt_height, gt_width)
 
+adv_pos = combine_discriminator(global_inputs=images,
+                                local_inputs=local_dis_inputs_real,
+                                is_training=is_training)
+adv_neg = combine_discriminator(global_inputs=global_dis_inputs_fake,
+                                local_inputs=local_dis_inputs_fake,
+                                is_training=is_training,
+                                reuse=True)
 
-loss_mse = tf.reduce_mean(tf.square(masks * (images - completed_images)))
+print(images.get_shape().as_list())
+print(images_with_hole.get_shape().as_list())
+print(completed_images.get_shape().as_list())
+print(global_dis_inputs_fake.get_shape().as_list())
+print(local_dis_inputs_fake.get_shape().as_list())
+print(local_dis_inputs_real.get_shape().as_list())
+print(adv_pos.get_shape().as_list())
+print(adv_neg.get_shape().as_list())
 
-
+# loss_mse = tf.reduce_mean(tf.square(masks_c * (images - completed_images)))
 train_path = pd.read_pickle(compress_path)
 train_path.index = range(len(train_path))
 train_path = train_path.ix[np.random.permutation(len(train_path))]
+image_paths = train_path[0:batch_size]['image_path'].values
 
-for t1 in range(iters_total):
-    # load a minibatch of images x from training data
-    # generate masks Mc with random holes for each image x in the minibatch
-    pass
+images_, images_with_hole_, masks_c_, x_locs_, y_locs_ = read_batch(image_paths)
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    a, b, c, d = sess.run([completed_images, global_dis_inputs_fake, local_dis_inputs_fake, local_dis_inputs_real],
+                          feed_dict={images: images_,
+                                     images_with_hole: images_with_hole_,
+                                     masks_c: masks_c_,
+                                     x_locs: x_locs_,
+                                     y_locs: y_locs_,
+                                     is_training: True})
+
+    plt.subplot(241)
+    plt.imshow((255. * (images_[0] + 1) / 2.).astype('uint8'))
+    plt.subplot(242)
+    plt.imshow((255. * (images_with_hole_[0] + 1) / 2.).astype('uint8'))
+    plt.subplot(243)
+    plt.imshow((255. * masks_c_[0]).astype('uint8'))
+    plt.subplot(244)
+    plt.imshow((255. * (completed_images[0] + 1) / 2.).astype('uint8'))
+    plt.subplot(245)
+    plt.imshow((255. * (global_dis_inputs_fake[0] + 1) / 2.).astype('uint8'))
+    plt.subplot(246)
+    plt.imshow((255. * (local_dis_inputs_fake[0] + 1) / 2.).astype('uint8'))
+    plt.subplot(247)
+    plt.imshow((255. * (local_dis_inputs_real[0] + 1) / 2.).astype('uint8'))
+
+    plt.show()
+
+
+# for t1 in range(iters_total):
+#     # load a minibatch of images x from training data
+#     # generate masks Mc with random holes for each image x in the minibatch
+#     pass
