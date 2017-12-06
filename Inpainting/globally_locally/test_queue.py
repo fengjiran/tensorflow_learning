d06b8a7cb4b58cf558e3641ae308d6bd471dea89
fengjiran <fengjiran@foxmail.com>
2017-12-06 15:03:12 +0800

    test queue

--- Inpainting/globally_locally/test_queue.py
+++ Inpainting/globally_locally/test_queue.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import platform
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 if platform.system() == 'Windows':
@@ -11,12 +12,34 @@ elif platform.system() == 'Linux':
     compress_path = '/home/richard/TensorFlow_Learning/Inpainting/globally_locally/imagenet_train_path_linux.pickle'
     model_path = '/home/richard/TensorFlow_Learning/Inpainting/globally_locally/models_without_adv_l1'
 
+# load the train sample paths
+train_path = pd.read_pickle(compress_path)
+np.random.seed(42)
+train_path.index = range(len(train_path))
+train_path = train_path.ix[np.random.permutation(len(train_path))]
+train_path = train_path[:]['image_path'].values.tolist()
+print(len(train_path))
+print(train_path[0:2])
+# train_path = ['A.jpg', 'B.jpg', 'C.jpg']
+with tf.Session() as sess:
+    filename_queue = tf.train.string_input_producer(train_path, shuffle=False, capacity=64)
+    reader = tf.WholeFileReader()
+    name, image = reader.read(filename_queue)
+    dataname = tf.train.batch([name], 2)
 
-g1 = tf.Graph()
+    sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
 
-print('tf.get_default_graph()=', tf.get_default_graph())
-print('g1                    =', g1)
+    try:
+        while not coord.should_stop():
+            print(sess.run(dataname))
+    except tf.errors.OutOfRangeError:
+        print('Done training -- epoch limit reached')
 
-with g1.as_default():
-    with tf.Session(graph=g1) as sess1:
-        pass
+    finally:
+        coord.request_stop()
+    coord.join(threads)
+
+
+print('done')
