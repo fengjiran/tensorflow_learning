899980f10b315073c20a09e293514bb8a1843bcc
fengjiran <fengjiran@foxmail.com>
2018-03-18 22:30:30 +0800

    create trainset path

--- Inpainting/GlobalLocalImageCompletion_TF/CelebA/create_path.py
+++ Inpainting/GlobalLocalImageCompletion_TF/CelebA/create_path.py
@@ -11,4 +11,12 @@ if platform.system() == 'Windows':
     compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\celeba_train_path_win.pickle'
 elif platform.system() == 'Linux':
     path = '/media/icie/b29b7268-50ad-4752-8e03-457669cab10a/CelebA/Img/img_align_celeba_png.7z/img_align_celeba_png'
-    compress_path=''
+    compress_path = '/home/richard/TensorFlow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/celeba_train_path_linux.pickle'
+
+if not os.path.exists(compress_path):
+    for filepath, _, _ in os.walk(path):
+        paths = glob(os.path.join(filepath, '*.png'))
+
+    paths = np.hstack(paths)
+    trainset = pd.DataFrame({'image_path': paths})
+    trainset.to_pickle(compress_path)
