1ca21bdc8d67bb1313e325ec1bb068636f742ba6
fengjiran <fengjiran@foxmail.com>
2018-03-18 21:34:25 +0800

    add platform info

---
+++ Inpainting/GlobalLocalImageCompletion_TF/CelebA/train.py
@@ -0,0 +1,19 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+import platform
+import pickle
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+if platform.system() == 'Windows':
+    pass
+
+elif platform.system() == 'Linux':
+    pass
+
+elif platform.system() == 'Darwin':
+    pass
