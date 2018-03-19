4dbc539c35c87fa18189d9a302643f2125853975
fengjiran <fengjiran@foxmail.com>
2018-03-19 09:58:48 +0800

    complete input parse function

--- Inpainting/GlobalLocalImageCompletion_TF/CelebA/train.py
+++ Inpainting/GlobalLocalImageCompletion_TF/CelebA/train.py
@@ -17,3 +17,49 @@ elif platform.system() == 'Linux':
 
 elif platform.system() == 'Darwin':
     pass
+
+
+def input_parse(img_path):
+    with tf.device('/cpu:0'):
+        low = 48
+        high = 96
+        image_height = 178
+        image_width = 178
+        gt_height = 96
+        gt_width = 96
+
+        img_file = tf.read_file(img_path)
+        img_decoded = tf.image.decode_image(img_file, channels=3)
+
+        img = tf.cast(img_decoded, tf.float32)
+        img /= 255.
+        img = tf.image.resize_image_with_crop_or_pad(img, image_height, image_width)
+
+        # input image range from -1 to 1
+        img = 2 * img - 1
+
+        ori_image = tf.identity(img)
+
+        hole_height, hole_width = np.random.randint(low, high, size=(2))
+        y = tf.random_uniform([], 0, image_height - hole_height, tf.int32)
+        x = tf.random_uniform([], 0, image_width - hole_width, tf.int32)
+
+        mask = tf.pad(tensor=tf.ones((hole_height, hole_width)),
+                      paddings=[[y, image_height - hole_height - y],
+                                [x, image_width - hole_width - x]])
+        mask = tf.reshape(mask, [image_height, image_width, 1])
+        mask = tf.concat([mask] * 3, 2)
+
+        image_with_hole = img * (1 - mask) + mask
+
+        # generate the location of 96*96 patch for local discriminator
+        x_loc = tf.random_uniform(shape=[],
+                                  minval=tf.reduce_max([0, x + hole_width - gt_width]),
+                                  maxval=tf.reduce_min([x, image_width - gt_width]) + 1,
+                                  dtype=tf.int32)
+        y_loc = tf.random_uniform(shape=[],
+                                  minval=tf.reduce_max([0, y + hole_height - gt_height]),
+                                  maxval=tf.reduce_min([y, image_height - gt_height]) + 1,
+                                  dtype=tf.int32)
+
+        return ori_image, image_with_hole, mask, x_loc, y_loc
