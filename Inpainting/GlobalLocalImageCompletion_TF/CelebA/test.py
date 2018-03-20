26e8deb80191ef51f06c56136dab905c89dfc840
fengjiran <fengjiran@foxmail.com>
2018-03-20 19:39:40 +0800

    add checkpoint path

--- Inpainting/GlobalLocalImageCompletion_TF/CelebA/test.py
+++ Inpainting/GlobalLocalImageCompletion_TF/CelebA/test.py
@@ -2,12 +2,15 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
+from models import completion_network
+
 drawing = False  # true if mouse is pressed
 ix, iy = -1, -1
 color = (255, 255, 255)
 size = 10
-checkpoint_path = ''
-img_path = '/Users/apple/Desktop/000013.png'
+checkpoint_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\models_without_adv_l2'
+# img_path = '/Users/apple/Desktop/000013.png'
+img_path = 'C:\\Users\\Richard\\Desktop\\000013.png'
 batch_size = 32
 
 
@@ -33,25 +36,30 @@ def erase_img(img):
             cv2.rectangle(mask, (x - size, y - size), (x + size, y + size), color, -1)
 
     cv2.namedWindow('image')
+    # cv2.namedWindow('mask')
     cv2.setMouseCallback('image', erase_rect)
     cv2.setMouseCallback('mask', erase_rect)
     mask = np.zeros(img.shape)
 
     while True:
-        img_show = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        cv2.imshow('image', img_show)
+        # img_show = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imshow('image', img)
+        # cv2.imshow('mask', mask)
         k = cv2.waitKey(1) & 0xFF
-        if k == 10:
+        if k == ord('q'):
             break
 
-    test_img = cv2.resize(img, (178, 178))
-    test_img = 2 * test_img / 255. - 1
-    test_mask = cv2.resize(mask, (178, 178)) / 255.
+    # test_img = cv2.resize(img, (178, 178))
+    # test_img = 2 * test_img / 255. - 1
+    # test_mask = cv2.resize(mask, (178, 178)) / 255.
+    test_img = 2 * img / 255. - 1
+    test_mask = mask / 255.
 
     test_img = test_img * (1 - test_mask) + test_mask
 
     cv2.destroyAllWindows()
-    return np.tile(test_img[np.newaxis, ...], [batch_size, 1, 1, 1]), np.tile(test_mask[np.newaxis, ...], [batch_size, 1, 1, 1])
+    return test_img, test_img
+    # return np.tile(test_img[np.newaxis, ...], [batch_size, 1, 1, 1]), np.tile(test_mask[np.newaxis, ...], [batch_size, 1, 1, 1])
 
 
 def test(sess, model):
@@ -81,7 +89,10 @@ if __name__ == '__main__':
     img = cv2.imread(img_path)
     print(img.shape)
     x, y = erase_img(img)
-    # cv2.imshow('img', img)
-    # k = cv2.waitKey(0)
-    # if k == 27:
-    #     cv2.destroyAllWindows()
+    # print(x.shape, y.shape)
+    height, width = x.shape[0], x.shape[1]
+
+    is_training = tf.placeholder(tf.bool)
+    test_image = tf.placeholder(tf.float32, [1, height, width, 3])
+    res_image = completion_network(test_image, is_training, 1)
+    print(res_image.get_shape())
