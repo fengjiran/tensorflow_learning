b4f94d578c40f1fabcfbd859ed97513731e65c72
fengjiran <fengjiran@foxmail.com>
2018-03-22 15:43:06 +0800

    add train paths

--- Inpainting/GlobalLocalImageCompletion_TF/CelebA/train_celeba_with_global_adv_l1.py
+++ Inpainting/GlobalLocalImageCompletion_TF/CelebA/train_celeba_with_global_adv_l1.py
@@ -11,3 +11,13 @@ import tensorflow as tf
 from utils import Conv2dLayer
 from utils import BatchNormLayer
 from utils import FCLayer
+from models import completion_network
+
+if platform.system() == 'Windows':
+    compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\celeba_train_path_win.pickle'
+    g_model_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\models_without_adv_l1'
+    model_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\models_with_global_adv_l1'
+elif platform.system() == 'Linux':
+    compress_path = '/home/richard/TensorFlow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/imagenet_train_path_linux.pickle'
+    g_model_path = '/home/richard/TensorFlow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/models_without_adv_l1'
+    model_path = '/home/richard/TensorFlow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/models_with_global_adv_l1'
