a4d2cdf7938b5fbc4f2d0fc9797c32da81988962
fengjiran <fengjiran@foxmail.com>
2018-03-22 15:31:49 +0800

    train with global discriminator

---
+++ Inpainting/GlobalLocalImageCompletion_TF/CelebA/train_celeba_with_global_adv_l1.py
@@ -0,0 +1,13 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+import pickle
+import platform
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from utils import Conv2dLayer
+from utils import BatchNormLayer
+from utils import FCLayer
