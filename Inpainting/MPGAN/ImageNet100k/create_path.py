07d14a53e489f3ead005984f1b9c42c38f866818
fengjiran <fengjiran@foxmail.com>
2018-04-13 22:55:37 +0800

    rename folder

--- Inpainting/MPGAN/ImageNet100k/create_path.py
+++ Inpainting/MPGAN/ImageNet100k/create_path.py
@@ -7,8 +7,9 @@ import numpy as np
 import pandas as pd
 
 if platform.system() == 'Windows':
-    path = 'F:\\Datasets\\CelebA\\Img\\img_align_celeba_png.7z\\img_align_celeba_png'
-    compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\celeba_train_path_win.pickle'
+    # path = 'F:\\Datasets\\CelebA\\Img\\img_align_celeba_png.7z\\img_align_celeba_png'
+    path = 'F:\\Datasets\\ImageNet_100K'
+    compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\MPGAN\\ImageNet100k\\imagenet100k_train_path_win.pickle'
 elif platform.system() == 'Linux':
     path = '/media/icie/b29b7268-50ad-4752-8e03-457669cab10a/CelebA/Img/img_align_celeba_png.7z/img_align_celeba_png'
     compress_path = '/home/richard/TensorFlow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/celeba_train_path_linux.pickle'
