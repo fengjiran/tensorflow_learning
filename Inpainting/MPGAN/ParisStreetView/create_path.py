1d3a8fb82249737f6442fe3a5cb46fe99d0b325f
fengjiran <fengjiran@foxmail.com>
2018-04-13 23:28:45 +0800

    create paris street view path file

--- Inpainting/MPGAN/ParisStreetView/create_path.py
+++ Inpainting/MPGAN/ParisStreetView/create_path.py
@@ -7,15 +7,20 @@ import numpy as np
 import pandas as pd
 
 if platform.system() == 'Windows':
-    path = 'F:\\Datasets\\CelebA\\Img\\img_align_celeba_png.7z\\img_align_celeba_png'
-    compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\celeba_train_path_win.pickle'
+    # path = 'F:\\Datasets\\CelebA\\Img\\img_align_celeba_png.7z\\img_align_celeba_png'
+    path = 'F:\\Datasets\\Paris_StreetView\\paris_train_original'
+    compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\MPGAN\\ParisStreetView\\parisstreetview_train_path_win.pickle'
 elif platform.system() == 'Linux':
-    path = '/media/icie/b29b7268-50ad-4752-8e03-457669cab10a/CelebA/Img/img_align_celeba_png.7z/img_align_celeba_png'
-    compress_path = '/home/richard/TensorFlow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/celeba_train_path_linux.pickle'
+    if platform.node() == 'icie-Precision-T7610':
+        path = '/home/icie/Paris_StreetView/paris_train_original'
+        compress_path = '/home/icie/richard/MPGAN/ParisStreetView/parisstreetview_train_path_linux_7610.pickle'
+    elif platform.node() == 'icie-Precision-Tower-7810':
+        path = '/home/richard/datasets/Paris_StreetView/paris_train_original'
+        compress_path = '/home/richard/TensorFlow_Learning/Inpainting/MPGAN/ParisStreetView/parisstreetview_train_path_linux_7810.pickle'
 
 if not os.path.exists(compress_path):
     for filepath, _, _ in os.walk(path):
-        paths = glob(os.path.join(filepath, '*.png'))
+        paths = glob(os.path.join(filepath, '*.JPG'))
 
     paths = np.hstack(paths)
     trainset = pd.DataFrame({'image_path': paths})
