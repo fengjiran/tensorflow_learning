9662e567c04b91675ac4391c460e2984c63dc2d3
fengjiran <fengjiran@foxmail.com>
2018-04-23 09:57:46 +0800

    train

--- Inpainting/MPGAN/ImageNet100k/mpgan_imagenet100k_global_local_l1.py
+++ Inpainting/MPGAN/ImageNet100k/mpgan_imagenet100k_global_local_l1.py
@@ -27,8 +27,8 @@ elif platform.system() == 'Linux':
         g_model_path = '/home/icie/richard/MPGAN/ImageNet100k/models_without_adv_l1'
         model_path = '/home/icie/richard/MPGAN/ImageNet100k/models_global_local_l1'
 
-isFirstTimeTrain = False
-# isFirstTimeTrain = True
+# isFirstTimeTrain = False
+isFirstTimeTrain = True
 batch_size = 4
 weight_decay_rate = 1e-4
 init_lr_g = 3e-4
