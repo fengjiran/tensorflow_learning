4bde9171ed915acefb3cd8e23b09822c9989118e
fengjiran <fengjiran@foxmail.com>
2018-04-24 00:33:16 +0800

    kernel=3

--- Inpainting/MPGAN/CelebA/mpgan_models.py
+++ Inpainting/MPGAN/CelebA/mpgan_models.py
@@ -150,37 +150,37 @@ def global_discriminator(images, is_training, reuse=None):
     conv_layers = []
     bn_layers = []
     with tf.variable_scope('global_discriminator', reuse=reuse):
-        conv1 = Conv2dLayer(images, [5, 5, 3, 64], stride=2, name='conv1')
+        conv1 = Conv2dLayer(images, [3, 3, 3, 64], stride=2, name='conv1')
         bn1_layer = BatchNormLayer(conv1.output, is_training, name='bn1')
         bn1 = tf.nn.leaky_relu(bn1_layer.output)
         conv_layers.append(conv1)
         bn_layers.append(bn1_layer)
 
-        conv2 = Conv2dLayer(bn1, [5, 5, 64, 128], stride=2, name='conv2')
+        conv2 = Conv2dLayer(bn1, [3, 3, 64, 128], stride=2, name='conv2')
         bn2_layer = BatchNormLayer(conv2.output, is_training, name='bn2')
         bn2 = tf.nn.leaky_relu(bn2_layer.output)
         conv_layers.append(conv2)
         bn_layers.append(bn2_layer)
 
-        conv3 = Conv2dLayer(bn2, [5, 5, 128, 256], stride=2, name='conv3')
+        conv3 = Conv2dLayer(bn2, [3, 3, 128, 256], stride=2, name='conv3')
         bn3_layer = BatchNormLayer(conv3.output, is_training, name='bn3')
         bn3 = tf.nn.leaky_relu(bn3_layer.output)
         conv_layers.append(conv3)
         bn_layers.append(bn3_layer)
 
-        conv4 = Conv2dLayer(bn3, [5, 5, 256, 512], stride=2, name='conv4')
+        conv4 = Conv2dLayer(bn3, [3, 3, 256, 512], stride=2, name='conv4')
         bn4_layer = BatchNormLayer(conv4.output, is_training, name='bn4')
         bn4 = tf.nn.leaky_relu(bn4_layer.output)
         conv_layers.append(conv4)
         bn_layers.append(bn4_layer)
 
-        conv5 = Conv2dLayer(bn4, [5, 5, 512, 512], stride=2, name='conv5')
+        conv5 = Conv2dLayer(bn4, [3, 3, 512, 512], stride=2, name='conv5')
         bn5_layer = BatchNormLayer(conv5.output, is_training, name='bn5')
         bn5 = tf.nn.leaky_relu(bn5_layer.output)
         conv_layers.append(conv5)
         bn_layers.append(bn5_layer)
 
-        conv6 = Conv2dLayer(bn5, [5, 5, 512, 512], stride=2, name='conv6')
+        conv6 = Conv2dLayer(bn5, [3, 3, 512, 512], stride=2, name='conv6')
         bn6_layer = BatchNormLayer(conv6.output, is_training, name='bn6')
         bn6 = tf.nn.leaky_relu(bn6_layer.output)
         conv_layers.append(conv6)
