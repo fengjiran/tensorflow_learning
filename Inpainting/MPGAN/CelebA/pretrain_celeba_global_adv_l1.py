cffa5a48e9b2425b5470a296c650d6c56af300e7
fengjiran <fengjiran@foxmail.com>
2018-05-09 10:49:17 +0800

    get image shape

--- Inpainting/MPGAN/CelebA/pretrain_celeba_global_adv_l1.py
+++ Inpainting/MPGAN/CelebA/pretrain_celeba_global_adv_l1.py
@@ -124,7 +124,7 @@ dataset = dataset.repeat()
 iterator = dataset.make_initializable_iterator()
 
 images, images_with_hole, masks, x_locs, y_locs, hole_heights, hole_widths = iterator.get_next()
-image_height, image_width = images.get_shape()[1], images.get_shape()[2]
+image_height, image_width = images.get_shape().as_list()[1], images.get_shape().as_list()[2]
 
 syn_images = completion_network(images_with_hole, is_training, batch_size)
 # completed_images = (1 - masks) * images + masks * syn_images
@@ -268,7 +268,7 @@ with tf.Session() as sess:
 
     while iters <= iters_c + iters_d:
         if iters <= iters_c:
-            _, loss_view_g, gs, lr_view_g = sess.run([train_op_only_g, loss_only_g, global_step_g, lr_d],
+            _, loss_view_g, gs, lr_view_g = sess.run([train_op_only_g, loss_only_g, global_step_g, lr_g],
                                                      feed_dict={is_training: True})
             print('Epoch: {}, Iter_g: {}, loss_g: {}, lr_g: {}'.format(
                 int(iters / num_batch) + 1,
