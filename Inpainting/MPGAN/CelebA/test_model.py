1390b8830e5865db60aeccd0690c5368aa06b618
fengjiran <fengjiran@foxmail.com>
2018-05-21 19:33:28 +0800

    add layers

---
+++ Inpainting/MPGAN/CelebA/test_model.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+from utils import Conv2dLayer
+from utils import DeconvLayer
+from utils import DilatedConv2dLayer
+from utils import FCLayer
