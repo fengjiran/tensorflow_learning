a2427d57306d7141d8b5414cb1390e669e6d8cfb
fengjiran <fengjiran@foxmail.com>
2018-05-21 19:50:20 +0800

    add conv layer

--- Inpainting/MPGAN/CelebA/test_model.py
+++ Inpainting/MPGAN/CelebA/test_model.py
@@ -6,3 +6,15 @@ from utils import Conv2dLayer
 from utils import DeconvLayer
 from utils import DilatedConv2dLayer
 from utils import FCLayer
+
+
+def completion_network(images, is_training, batch_size):
+    """Construct completion network."""
+    # batch_size = images.get_shape().as_list()[0]
+    conv_layers = []
+    cnum = 32
+    input_channel = images.get_shape().as_list()[3]
+
+    with tf.variable_scope('generator'):
+        conv1 = Conv2dLayer(images, [5, 5, input_channel, cnum], stride=1, name='conv1')
+        conv2 = Conv2dLayer(conv1.output, [3, 3, cnum, 2 * cnum], stride=2, name='conv2')
