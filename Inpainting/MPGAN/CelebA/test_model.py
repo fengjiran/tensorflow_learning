bbac2083c0f26d5055ccde53aeec086e0298df72
fengjiran <fengjiran@foxmail.com>
2018-05-23 13:47:44 +0800

    add

--- Inpainting/MPGAN/CelebA/test_model.py
+++ Inpainting/MPGAN/CelebA/test_model.py
@@ -2,10 +2,69 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.framework import ops
 from utils import Conv2dLayer
 from utils import DeconvLayer
 from utils import DilatedConv2dLayer
 # from utils import FCLayer
+
+
+def coarse_network_(images):
+    """Construct coarse network."""
+    # batch_size = images.get_shape().as_list()[0]
+    conv_layers = []
+    cnum = 32
+    # input_channel = images.get_shape().as_list()[3]
+
+    with tf.variable_scope('coarse'):
+        conv1 = tf.layers.conv2d(images, cnum, 5, strides=1, padding='same', activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv1')
+        conv2 = tf.layers.conv2d(conv1, 2 * cnum, 3, strides=2, padding='same', activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv2_downsample')
+        conv3 = tf.layers.conv2d(conv2, 2 * cnum, 3, strides=1, padding='same', activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv3')
+        conv4 = tf.layers.conv2d(conv3, 4 * cnum, 3, strides=2, padding='same', activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv4_downsample')
+        conv5 = tf.layers.conv2d(conv4, 4 * cnum, 3, strides=1, padding='same', activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv5')
+        conv6 = tf.layers.conv2d(conv5, 4 * cnum, 3, strides=1, padding='same', activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv6')
+
+        conv7 = tf.layers.conv2d(conv6, 4 * cnum, 3, padding='same', dilation_rate=2, activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv7_atrous')
+        conv8 = tf.layers.conv2d(conv7, 4 * cnum, 3, padding='same', dilation_rate=4, activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv8_atrous')
+        conv9 = tf.layers.conv2d(conv8, 4 * cnum, 3, padding='same', dilation_rate=8, activation=tf.nn.elu,
+                                 kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv9_atrous')
+        conv10 = tf.layers.conv2d(conv9, 4 * cnum, 3, padding='same', dilation_rate=16, activation=tf.nn.elu,
+                                  kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv10_atrous')
+
+        conv11 = tf.layers.conv2d(conv10, 4 * cnum, 3, strides=1, padding='same', activation=tf.nn.elu,
+                                  kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv11')
+        conv12 = tf.layers.conv2d(conv11, 4 * cnum, 3, strides=1, padding='same', activation=tf.nn.elu,
+                                  kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv12')
+
+        conv13 = tf.layers.conv2d_transpose(conv12, 2 * cnum, 3, strides=2, padding='same', activation=tf.nn.elu,
+                                            kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv13_upsample')
+        conv14 = tf.layers.conv2d(conv13, 2 * cnum, 3, strides=1, padding='same', activation=tf.nn.elu,
+                                  kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv14')
+        conv15 = tf.layers.conv2d_transpose(conv14, cnum, 3, strides=2, padding='same', activation=tf.nn.elu,
+                                            kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv15_upsample')
+
+        conv16 = tf.layers.conv2d(conv15, int(cnum / 2), 3, strides=1, padding='same', activation=tf.nn.elu,
+                                  kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv16')
+        conv17 = tf.layers.conv2d(conv16, 3, 3, strides=1, padding='same',
+                                  kernel_initializer=tf.keras.initializers.glorot_normal(), name='conv17')
+
+        conv_output = tf.clip_by_value(conv17, -1., 1.)
+
+        for i in range(1, 18):
+            conv_layers.append(eval('conv{}'.format(i)))
+
+        for conv in conv_layers:
+            print('conv:{}, output_shape:{}'.format(conv_layers.index(conv) + 1, conv.get_shape().as_list()))
+
+        return conv_output
 
 
 def coarse_network(images, batch_size):
@@ -268,6 +327,7 @@ def spatial_discounting_mask(gamma, height, width):
 
 
 def random_bbox(image_shape, hole_height, hole_width):
+    # image_shape:(H,W,C)
     height = image_shape[0]
     width = image_shape[1]
 
@@ -353,11 +413,52 @@ def gradient_penalty(x, y, mask=None, norm=1.):
     return tf.reduce_mean(tf.square(slopes - norm))
 
 
+def collection_to_dict(collection):
+    """Utility function to construct collection dict with names."""
+    return {v.name[:v.name.rfind(':')]: v for v in collection}
+
+
+def scalar_summary(name, value, sess=None, summary_writer=None, step=None):
+    """Add scalar summary.
+
+    In addition to summary tf.Tensor and tf.Variable, this function supports
+    summary of constant values by creating placeholder.
+    Example usage:
+    >>> scalar_summary('lr', lr)
+    :param name: name of summary variable
+    :param value: numpy or tensorflow tensor
+    :param summary_writer: if summary writer is provided, write to summary
+        instantly
+    :param step: if summary writer is provided, write to summary with step
+    :return: None
+
+    """
+    def is_tensor_or_var(value):
+        return isinstance(value, tf.Tensor) or isinstance(value, tf.Variable)
+
+    # get scope name
+    sname = tf.get_variable_scope().name
+    fname = name if not sname else sname + '/' + name
+
+    # construct summary dict
+    collection = collection_to_dict(ops.get_collection(ops.GraphKeys.SUMMARIES))
+
+    # tensorflow tensor
+    if fname in collection:
+        if not is_tensor_or_var(value):
+            ph_collection = collection_to_dict(tf.get_collection('SUMMARY_PLACEHOLDERS'))
+            op_collection = collection_to_dict(tf.get_collection('SUMMARY_OPS'))
+            ph = ph_collection[fname + '_ph']
+            op = op_collection[fname + '_op']
+
+
 if __name__ == '__main__':
-    x = tf.random_uniform([10, 256, 256, 3])
-    # y = refine_network(x, 10)
+    x = tf.random_uniform([10, 178, 218, 3])
+    y = coarse_network_(x)
     image_shape = (256, 256, 3)
     bbox = (5, 5, 128, 128)
     # mask = bbox2mask(image_shape, bbox)
-    y = local_patch(x, bbox)
-    print(y.get_shape())
+    # y = local_patch(x, bbox)
+    # print(y.get_shape())
+    # g_vars, d_vars, losses = build_graph_with_losses(x, image_shape, 128, 128)
+    # print(len(g_vars), len(d_vars))
