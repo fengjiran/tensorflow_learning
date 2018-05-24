9c4c99388fd99dc8260a856b15bf4f3fa3b42303
fengjiran <fengjiran@foxmail.com>
2018-05-24 11:42:33 +0800

    input parse

--- Inpainting/contextual_attention/train.py
+++ Inpainting/contextual_attention/train.py
@@ -22,3 +22,19 @@ elif platform.system() == 'Linux':
         compress_path = '/home/icie/richard/MPGAN/CelebA/celeba_train_path_linux.pickle'
         # events_path = '/home/icie/richard/MPGAN/CelebA/models_without_adv_l1/events'
         # model_path = '/home/icie/richard/MPGAN/CelebA/pretrain_model_global'
+
+
+def input_parse(img_path):
+    with tf.device('/cpu:0'):
+        img_file = tf.read_file(img_path)
+        img_decoded = tf.image.decode_image(img_file, channels=3)
+        img = tf.cast(img_decoded, tf.float32)
+        img = tf.image.resize_images(img, [315, 256])
+        img = tf.image.random_crop(img, [cfg['img_height'], cfg['img_width']])
+        img = img / 127.5 - 1
+
+        return img
+
+
+model = CompletionModel()
+g_vars, d_vars, losses = model.build_graph_with_losses(images, cfg)
