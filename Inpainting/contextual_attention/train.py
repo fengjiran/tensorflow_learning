a7f5ae9a6f8a4a1051a653cc0c4596c1f6f6ac3a
fengjiran <fengjiran@foxmail.com>
2018-05-24 20:31:48 +0800

    train

--- Inpainting/contextual_attention/train.py
+++ Inpainting/contextual_attention/train.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 
 import platform
 import yaml
-# import numpy as np
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 from model import CompletionModel
 
@@ -10,14 +11,14 @@ with open('config.yaml', 'r') as f:
     cfg = yaml.load(f)
 
 if platform.system() == 'Windows':
-    compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\MPGAN\\CelebA\\celeba_train_path_win.pickle'
+    compress_path = cfg['compress_path_win']
     log_dir = cfg['log_dir_win']
-    # model_path = 'E:\\TensorFlow_Learning\\Inpainting\\MPGAN\\CelebA\\pretrain_model_global'
+    model_path = cfg['model_path_win']
 elif platform.system() == 'Linux':
     if platform.node() == 'icie-Precision-Tower-7810':
-        compress_path = '/home/richard/TensorFlow_Learning/Inpainting/MPGAN/CelebA/celeba_train_path_linux.pickle'
+        compress_path = cfg['compress_path_linux']
         log_dir = cfg['log_dir_linux']
-        # model_path = '/home/richard/TensorFlow_Learning/Inpainting/MPGAN/CelebA/pretrain_model_global'
+        model_path = cfg['model_path_linux']
     elif platform.node() == 'icie-Precision-T7610':
         compress_path = '/home/icie/richard/MPGAN/CelebA/celeba_train_path_linux.pickle'
         # events_path = '/home/icie/richard/MPGAN/CelebA/models_without_adv_l1/events'
@@ -71,3 +72,23 @@ g_train = g_opt.apply_gradients(g_grads_vars, global_step)
 
 d_grads_vars = d_opt.compute_gradients(d_loss, d_vars)
 d_train = d_opt.apply_gradients(d_grads_vars, global_step)
+
+saver = tf.train.Saver()
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config) as sess:
+    # load trainset
+    train_path = pd.read_pickle(compress_path)
+    train_path.index = range(len(train_path))
+    train_path = train_path.ix[np.random.permutation(len(train_path))]
+    train_path = train_path[:]['image_path'].values.tolist()
+    num_batch = int(len(train_path) / cfg['batch_size'])
+
+    sess.run(iterator.initializer, feed_dict={filenames: train_path})
+    sess.run(tf.global_variables_initializer())
+
+    step = 0
+    max_iters = cfg['max_iters']
+    while step < max_iters:
+        _, loss_value = sess.run([g_train, losses['g_loss']])
