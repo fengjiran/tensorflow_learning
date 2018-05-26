2aefefda0e1015d602a604c4aea88363da938d2c
fengjiran <fengjiran@foxmail.com>
2018-05-26 23:33:04 +0800

    d loss

--- Inpainting/contextual_attention/model.py
+++ Inpainting/contextual_attention/model.py
@@ -256,7 +256,7 @@ class CompletionModel(object):
         g_loss_local, d_loss_local = gan_wgan_loss(pos_local, neg_local)
 
         losses['refine_g_loss'] = cfg['global_wgan_loss_alpha'] * g_loss_global + g_loss_local
-        losses['refine_d_loss'] = g_loss_global + d_loss_local
+        losses['refine_d_loss'] = d_loss_global + d_loss_local
 
         # gradient penalty
         interpolates_global = random_interpolates(batch_pos, batch_complete_refine)
