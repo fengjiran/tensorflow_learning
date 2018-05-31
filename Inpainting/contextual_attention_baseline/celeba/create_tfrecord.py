f7066c7403f7d4f91c2d649d7fd7783be33715ac
fengjiran <fengjiran@foxmail.com>
2018-05-31 16:12:31 +0800

    create testset

--- Inpainting/contextual_attention_baseline/celeba/create_tfrecord.py
+++ Inpainting/contextual_attention_baseline/celeba/create_tfrecord.py
@@ -55,7 +55,8 @@ def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
     image_filenames = sorted(glob.glob(glob_pattern))
     # print(len(image_filenames))
     # expected_images = 202599
-    expected_images = 182637
+    # expected_images = 182637
+    expected_images = 9981
     # if len(image_filenames) != expected_images:
     #     error('Expected to find %d images' % expected_images)
 
@@ -63,13 +64,13 @@ def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
     # tfrecord_num = 1
     # tfrecord_name = 'celeba_traindata.tfrecord-%.3d' % tfrecord_num
     num_tfrecords = expected_images // num
-    order = np.arange(expected_images)
+    order = np.arange(expected_images) + 182637 + 9981
     # np.random.RandomState(123).shuffle(order)
 
     cur_img = 1
     for i in range(num_tfrecords - 1):
         print('write ', i + 1, ' file')
-        tfrecordname = os.path.join(tfrecord_dir, 'celeba_trainset.tfrecord-%.3d' % (i + 1))
+        tfrecordname = os.path.join(tfrecord_dir, 'celeba_testset.tfrecord-%.3d' % (i + 1))
         writer = tf.python_io.TFRecordWriter(path=tfrecordname)
         for j in range(num):
             print('write ', cur_img, ' image')
@@ -89,11 +90,11 @@ def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
         writer.close()
 
     print('write ', num_tfrecords, ' file')
-    tfrecordname = os.path.join(tfrecord_dir, 'celeba_trainset.tfrecord-%.3d' % num_tfrecords)
+    tfrecordname = os.path.join(tfrecord_dir, 'celeba_testset.tfrecord-%.3d' % num_tfrecords)
     writer = tf.python_io.TFRecordWriter(path=tfrecordname)
     for idx in order[(num_tfrecords - 1) * num:]:
         print('write ', cur_img, ' image')
-        img = np.asarray(PIL.Image.open(image_filenames[order[idx]]))
+        img = np.asarray(PIL.Image.open(image_filenames[idx]))
         assert img.shape == (218, 178, 3)
         img = img.astype(np.float32)
         img = img / 127.5 - 1
@@ -111,5 +112,5 @@ def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
 
 if __name__ == '__main__':
     celeba_dir = 'F:\\Datasets\\CelebA\\Img\\img_align_celeba_png.7z\\img_align_celeba_png'
-    tfrecord_dir = 'F:\\Datasets\\celeba_tfrecords'
+    tfrecord_dir = 'F:\\Datasets\\celeba_tfrecords\\test'
     create_celeba_tfrecord(tfrecord_dir, celeba_dir)
