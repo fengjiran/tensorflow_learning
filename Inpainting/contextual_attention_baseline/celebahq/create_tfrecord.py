56581d4a99b4b457ef8b6d55d65496820337b6c3
fengjiran <fengjiran@foxmail.com>
2018-06-04 00:50:04 +0800

    create

--- Inpainting/contextual_attention_baseline/celebahq/create_tfrecord.py
+++ Inpainting/contextual_attention_baseline/celebahq/create_tfrecord.py
@@ -12,51 +12,15 @@ def error(msg):
     exit(1)
 
 
-class TFRecordExporter(object):
-    """Create tfrecord dataset."""
-
-    def __init__(self, tfrecord_dir, expected_images,
-                 print_progress=True, progress_interval=10):
-        self.tfrecord_dir = tfrecord_dir
-        self.expected_images = expected_images
-        self.tfr_prefix = os.path.join(self.tfrecord_dir, os.path.basename(self.tfrecord_dir))
-        self.cur_images = 0
-        self.shape = None
-        self.tfr_writers = []
-        self.print_progress = print_progress
-        self.progress_interval = progress_interval
-
-        if self.print_progress:
-            print('Creating dataset "%s"' % tfrecord_dir)
-        if not os.path.isdir(self.tfrecord_dir):
-            os.makedirs(self.tfrecord_dir)
-        assert os.path.isdir(self.tfrecord_dir)
-
-    def close(self):
-        if self.print_progress:
-            print('%-40s\r' % 'Flushing data...', end='', flush=True)
-        for tfr_writer in self.tfr_writers:
-            tfr_writer.close()
-        self.tfr_writers = []
-        if self.print_progress:
-            print('%-40s\r' % '', end='', flush=True)
-            print('Added %d images.' % self.cur_images)
-
-    def choose_shuffled_order(self):  # Note: Images and labels must be added in shuffled order.
-        order = np.arange(self.expected_images)
-        np.random.RandomState(123).shuffle(order)
-        return order
-
-
-def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
+def create_celebahq_tfrecord(tfrecord_dir, celebahq_dir):
     num = 1000
-    print('Loading CelebA from "%s"' % celeba_dir)
-    glob_pattern = os.path.join(celeba_dir, '*.png')
+    print('Loading CelebA from "%s"' % celebahq_dir)
+    glob_pattern = os.path.join(celebahq_dir, '*.png')
     image_filenames = sorted(glob.glob(glob_pattern))
     # print(len(image_filenames))
     # expected_images = 202599
     # expected_images = 182637
-    expected_images = 9981
+    expected_images = 29000
     # if len(image_filenames) != expected_images:
     #     error('Expected to find %d images' % expected_images)
 
@@ -64,20 +28,20 @@ def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
     # tfrecord_num = 1
     # tfrecord_name = 'celeba_traindata.tfrecord-%.3d' % tfrecord_num
     num_tfrecords = expected_images // num
-    order = np.arange(expected_images) + 182637 + 9981
-    # np.random.RandomState(123).shuffle(order)
+    order = np.arange(expected_images)
+    np.random.RandomState(123).shuffle(order)
 
     cur_img = 1
     for i in range(num_tfrecords - 1):
         print('write ', i + 1, ' file')
-        tfrecordname = os.path.join(tfrecord_dir, 'celeba_testset.tfrecord-%.3d' % (i + 1))
+        tfrecordname = os.path.join(tfrecord_dir, 'celebahq_trainset.tfrecord-%.3d' % (i + 1))
         writer = tf.python_io.TFRecordWriter(path=tfrecordname)
         for j in range(num):
             print('write ', cur_img, ' image')
             img = np.asarray(PIL.Image.open(image_filenames[order[i * num + j]]))
-            assert img.shape == (218, 178, 3)
-            img = img.astype(np.float32)
-            img = img / 127.5 - 1
+            assert img.shape == (1024, 1024, 3)
+            # img = img.astype(np.float32)
+            # img = img / 127.5 - 1
             example = tf.train.Example(
                 features=tf.train.Features(
                     feature={
@@ -90,14 +54,14 @@ def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
         writer.close()
 
     print('write ', num_tfrecords, ' file')
-    tfrecordname = os.path.join(tfrecord_dir, 'celeba_testset.tfrecord-%.3d' % num_tfrecords)
+    tfrecordname = os.path.join(tfrecord_dir, 'celebahq_trainset.tfrecord-%.3d' % num_tfrecords)
     writer = tf.python_io.TFRecordWriter(path=tfrecordname)
     for idx in order[(num_tfrecords - 1) * num:]:
         print('write ', cur_img, ' image')
         img = np.asarray(PIL.Image.open(image_filenames[idx]))
-        assert img.shape == (218, 178, 3)
-        img = img.astype(np.float32)
-        img = img / 127.5 - 1
+        assert img.shape == (1024, 1024, 3)
+        # img = img.astype(np.float32)
+        # img = img / 127.5 - 1
         example = tf.train.Example(
             features=tf.train.Features(
                 feature={
@@ -111,6 +75,6 @@ def create_celeba_tfrecord(tfrecord_dir, celeba_dir):
 
 
 if __name__ == '__main__':
-    celeba_dir = 'F:\\Datasets\\CelebA\\Img\\img_align_celeba_png.7z\\img_align_celeba_png'
-    tfrecord_dir = 'F:\\Datasets\\celeba_tfrecords\\test'
-    create_celeba_tfrecord(tfrecord_dir, celeba_dir)
+    celebahq_dir = 'F:\\Datasets\\celebahq'
+    tfrecord_dir = 'F:\\Datasets\\celebahq_tfrecords\\train'
+    create_celebahq_tfrecord(tfrecord_dir, celebahq_dir)
