088c1d3fc808e889a3da38dcf5c1454e0dcd9354
fengjiran <fengjiran@foxmail.com>
2018-06-05 14:19:14 +0800

    train

--- Inpainting/contextual_attention_baseline/celeba/train.py
+++ Inpainting/contextual_attention_baseline/celeba/train.py
@@ -49,8 +49,7 @@ def parse_tfrecord(example_proto):
     parsed_features = tf.parse_single_example(example_proto, features)
     data = tf.decode_raw(parsed_features['data'], tf.float32)
     img = tf.reshape(data, parsed_features['shape'])
-    img = tf.image.resize_area(img, [315, 256])
-    img = tf.clip_by_value(img, 0., 255.)
+    img = tf.image.resize_images(img, [315, 256])
     img = tf.random_crop(img, [cfg['img_height'], cfg['img_width'], 3])
     img = img / 127.5 - 1
 
@@ -166,14 +165,6 @@ if cfg['val']:
         val_batch_data,
         cfg,
         'static_images')
-    # for i in range(cfg['static_view_num']):
-    #     static_fname = val_path[i]
-    #     static_image = input_parse(static_fname)
-    #     static_image = tf.expand_dims(static_image, 0)
-    #     static_inpainted_image = model.build_static_infer_graph(
-    #         static_image,
-    #         cfg,
-    #         'static_view/%d' % i)
 
 # summary
 tf.summary.scalar('learning_rate/lr_g', lr_g)
