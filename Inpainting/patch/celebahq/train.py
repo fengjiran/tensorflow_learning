c4a127528c7a7eb344c9e5b82c7ba63b0a2360ee
fengjiran <fengjiran@foxmail.com>
2018-06-19 17:36:26 +0800

    train

--- Inpainting/patch/celebahq/train.py
+++ Inpainting/patch/celebahq/train.py
@@ -189,8 +189,8 @@ with tf.Session(config=config) as sess:
         step = 0
         sess.run(tf.global_variables_initializer())
     else:
-        # saver.restore(sess, os.path.join(refine_model_path, 'refine_model'))
-        saver.restore(sess, os.path.join(coarse_model_path, 'coarse_model'))
+        saver.restore(sess, os.path.join(refine_model_path, 'refine_model'))
+        # saver.restore(sess, os.path.join(coarse_model_path, 'coarse_model'))
         step = global_step_g.eval()
 
     total_iters = cfg['total_iters']
@@ -220,7 +220,7 @@ with tf.Session(config=config) as sess:
             if (step % 200 == 0) or (step == cfg['total_iters'] - 1):
                 saver.save(sess, os.path.join(refine_model_path, 'refine_model'))
 
-        if (step % 100 == 0) or (step == cfg['total_iters'] - 1):
+        if (step % 200 == 0) or (step == cfg['total_iters'] - 1):
             summary = sess.run(all_summary)
             summary_writer.add_summary(summary, step)
 
