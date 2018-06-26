3c765435c5e4b38a5cfac51cacc2bca3bcd09603
fengjiran <fengjiran@foxmail.com>
2018-06-26 11:25:46 +0800

    add

---
+++ test/ytvd.py
@@ -0,0 +1,21 @@
+import sys
+from pytube import YouTube
+
+
+def download_main(link):
+    video_format = 'mp4'
+    yt = YouTube(link).streams
+    obj = yt.filter(video_format)[-1]  # select the highest resolution
+    resolution = obj.resolution    # obtain the video resolution
+    video = yt.get(video_format, resolution)
+    video.download('F:\\YouTube_download')
+
+
+if __name__ == '__main__':
+    link = 'https://www.youtube.com/watch?v=RKWuj1OlDPo'
+    YouTube(link).streams.first().download('F:\\YouTube_download')
+    # download_main(sys.argv[1])
+    # for link in links_lizhi[11:]:
+    #     download_main(link)
+
+    # download_main('https://www.youtube.com/watch?v=Br5aJa6MkBc')
