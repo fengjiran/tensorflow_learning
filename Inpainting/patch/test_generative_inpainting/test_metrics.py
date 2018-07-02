df627c98299f6b107c95d53d3154fe2b95247627
fengjiran <fengjiran@foxmail.com>
2018-07-02 10:55:10 +0800

    test tensor shape

--- Inpainting/patch/test_generative_inpainting/test_metrics.py
+++ Inpainting/patch/test_generative_inpainting/test_metrics.py
@@ -30,9 +30,9 @@ def bbox2mask(bbox, height, width):
                   paddings=[[top, height - h - top],
                             [left, width - w - left]])
 
-    mask = tf.expand_dims(mask, 0)
-    # mask = tf.expand_dims(mask, -1)
-    mask = tf.concat([mask, mask, mask], axis=3)
+    # mask = tf.expand_dims(mask, 0)
+    mask = tf.expand_dims(mask, -1)
+    mask = tf.concat([mask, mask, mask], axis=2)
     return mask
 
 
@@ -65,7 +65,7 @@ val_batch_data = val_iterator.get_next()
 val_batch_data = tf.image.resize_area(val_batch_data, [256, 256])
 # val_batch_data = tf.clip_by_value(val_batch_data, 0., 255.)
 val_batch_data = val_batch_data / 127.5 - 1
-val_batch_data = tf.reshape(val_batch_data, [256, 256, 3])
+# val_batch_data = tf.reshape(val_batch_data, [256, 256, 3])
 
 hole_size = 16
 image_size = 256
@@ -74,9 +74,12 @@ bbox = (tf.constant((image_size - hole_size) // 2),
         tf.constant(hole_size),
         tf.constant(hole_size))
 
-mask = bbox2mask(bbox, image_size, image_size)
-ones_x = tf.ones_like(val_batch_data)
-mask = ones_x * mask
+mask = bbox2mask(bbox, image_size, image_size)  # (256,256,3)
+mask = tf.expand_dims(mask, 0)
+print(mask.get_shape())
+print(val_batch_data.get_shape())
+# ones_x = tf.ones_like(val_batch_data)
+# mask = ones_x * mask
 input_image = tf.concat([val_batch_data, mask], axis=2)
 # print(mask.get_shape())
 print(input_image.get_shape())
@@ -88,4 +91,6 @@ sess_config = tf.ConfigProto()
 sess_config.gpu_options.allow_growth = True
 with tf.Session(config=sess_config) as sess:
     # input_image = tf.constant(input_image, dtype=tf.float32)
-    output = model.build_server_graph(input_image)
+    # output = model.build_server_graph(input_image)
+    a = sess.run(input_image)
+    print(a.shape)
