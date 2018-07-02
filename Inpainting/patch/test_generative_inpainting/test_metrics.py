d8835696f81b837b48c3d286d6e505b5d6fdfe57
fengjiran <fengjiran@foxmail.com>
2018-07-02 10:37:45 +0800

    reshape tensor

--- Inpainting/patch/test_generative_inpainting/test_metrics.py
+++ Inpainting/patch/test_generative_inpainting/test_metrics.py
@@ -65,6 +65,7 @@ val_batch_data = val_iterator.get_next()
 val_batch_data = tf.image.resize_area(val_batch_data, [256, 256])
 val_batch_data = tf.clip_by_value(val_batch_data, 0., 255.)
 val_batch_data = val_batch_data / 127.5 - 1
+val_batch_data = tf.reshape(val_batch_data, [256, 256, 3])
 
 hole_size = 16
 image_size = 256
