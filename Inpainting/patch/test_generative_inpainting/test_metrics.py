fc6825d968c493a0e462e61070090f82939bbde7
fengjiran <fengjiran@foxmail.com>
2018-07-03 16:49:52 +0800

    test

--- Inpainting/patch/test_generative_inpainting/test_metrics.py
+++ Inpainting/patch/test_generative_inpainting/test_metrics.py
@@ -77,9 +77,30 @@ bbox_np = ((image_size - hole_size) // 2,
            hole_size)
 mask = bbox2mask_np(bbox_np, image_size, image_size)
 
-for i in range(29000, 30000):
-    img_path = os.path.join(prefix, 'img%.8d.png' % i)
+# model = InpaintCAModel()
+# sess_config = tf.ConfigProto()
+# sess_config.gpu_options.allow_growth = True
+# with tf.Session(config=sess_config) as sess:
+#     vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+#     assign_ops = []
+#     for var in vars_list:
+#         vname = var.name
+#         from_name = vname
+#         var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
+#         assign_ops.append(tf.assign(var, var_value))
+#     sess.run(assign_ops)
+#     print('Model loaded.')
+
+for i in range(1000):
+    img_path = os.path.join(prefix, 'img%.8d.png' % (i + 29000))
     image = cv2.imread(img_path)
+    image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
+    image = np.expand_dims(image, 0)
+    assert image.shape == mask.shape  # (1,256,256,3)
+
+    input_image = np.concatenate([image, mask], axis=2)
+
+
 # val_filenames = tf.placeholder(tf.string)
 # val_data = tf.data.TFRecordDataset(val_filenames)
 # val_data = val_data.map(parse_tfrecord)
@@ -94,77 +115,77 @@ for i in range(29000, 30000):
 # val_batch_data = tf.reshape(val_batch_data, [256, 256, 3])
 
 
-bbox = (tf.constant((image_size - hole_size) // 2),
-        tf.constant((image_size - hole_size) // 2),
-        tf.constant(hole_size),
-        tf.constant(hole_size))
+# bbox = (tf.constant((image_size - hole_size) // 2),
+#         tf.constant((image_size - hole_size) // 2),
+#         tf.constant(hole_size),
+#         tf.constant(hole_size))
 
 # mask = bbox2mask(bbox, image_size, image_size) * 255  # (256,256,3)
 # mask = tf.expand_dims(mask, 0)  # (1,256,256,3)
 # print(mask.get_shape())
 # print(val_batch_data.get_shape())
 
-input_image = tf.concat([val_batch_data, mask], axis=2)  # (1,256,512,3)
+# input_image = tf.concat([val_batch_data, mask], axis=2)  # (1,256,512,3)
 # input_image = tf.cast(input_image, tf.float32)
 # input_image = tf.constant(input_image, dtype=tf.float32)
 
 # print(input_image.get_shape())
 # ng.get_gpus(1)
 # args = parser.parse_args()
-model = InpaintCAModel()
+# model = InpaintCAModel()
 # output = model.build_server_graph(input_image)
 
-sess_config = tf.ConfigProto()
-sess_config.gpu_options.allow_growth = True
-with tf.Session(config=sess_config) as sess:
-    sess.run(val_iterator.initializer, feed_dict={val_filenames: val_path})
-    # input_image = sess.run(input_image)
-    # val, _ = np.split(input_image, 2, axis=2)
-    # val = sess.run(val_batch_data)
-    # val = (val + 1) * 127.5
-    # val = np.reshape(val, (256, 256, 3))
-    # val = val.astype(np.uint8)
-    # cv2.imwrite('F:\\val.png', val[:, :, ::-1])
+# sess_config = tf.ConfigProto()
+# sess_config.gpu_options.allow_growth = True
+# with tf.Session(config=sess_config) as sess:
+#     sess.run(val_iterator.initializer, feed_dict={val_filenames: val_path})
+#     # input_image = sess.run(input_image)
+#     # val, _ = np.split(input_image, 2, axis=2)
+#     # val = sess.run(val_batch_data)
+#     # val = (val + 1) * 127.5
+#     # val = np.reshape(val, (256, 256, 3))
+#     # val = val.astype(np.uint8)
+#     # cv2.imwrite('F:\\val.png', val[:, :, ::-1])
 
-    # input_image = tf.constant(input_image, dtype=tf.float32)
-    output = model.build_server_graph(input_image)  # (-1, 1)
-    output = (output + 1.) * 127.5  # (0, 255)
-    output = tf.saturate_cast(output, tf.uint8)
-    output = tf.reverse(output, [-1])
+#     # input_image = tf.constant(input_image, dtype=tf.float32)
+#     output = model.build_server_graph(input_image)  # (-1, 1)
+#     output = (output + 1.) * 127.5  # (0, 255)
+#     output = tf.saturate_cast(output, tf.uint8)
+#     output = tf.reverse(output, [-1])
 
-    ssim = tf.image.ssim(tf.saturate_cast(val_batch_data[0][:, :, ::-1], tf.uint8), output[0], 255)
-    psnr = tf.image.psnr(tf.saturate_cast(val_batch_data[0][:, :, ::-1], tf.uint8), output[0], 255)
+#     ssim = tf.image.ssim(tf.saturate_cast(val_batch_data[0][:, :, ::-1], tf.uint8), output[0], 255)
+#     psnr = tf.image.psnr(tf.saturate_cast(val_batch_data[0][:, :, ::-1], tf.uint8), output[0], 255)
 
-    # tmp1 = val_batch_data[0] / 127.5 - 1
-    # tmp2 = tf.cast(output[0], tf.float32) / 127.5 - 1
+#     # tmp1 = val_batch_data[0] / 127.5 - 1
+#     # tmp2 = tf.cast(output[0], tf.float32) / 127.5 - 1
 
-    # l1_loss = tf.reduce_mean(tf.abs(tmp1 - tmp2))
-    # l2_loss = tf.reduce_mean(tf.square(tmp1 - tmp2))
+#     # l1_loss = tf.reduce_mean(tf.abs(tmp1 - tmp2))
+#     # l2_loss = tf.reduce_mean(tf.square(tmp1 - tmp2))
 
-    # load pretrained model
-    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-    assign_ops = []
-    for var in vars_list:
-        vname = var.name
-        from_name = vname
-        var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
-        assign_ops.append(tf.assign(var, var_value))
-    sess.run(assign_ops)
-    print('Model loaded.')
+#     # load pretrained model
+#     vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+#     assign_ops = []
+#     for var in vars_list:
+#         vname = var.name
+#         from_name = vname
+#         var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
+#         assign_ops.append(tf.assign(var, var_value))
+#     sess.run(assign_ops)
+#     print('Model loaded.')
 
-    result, val, ssim, psnr = sess.run([output, val_batch_data, ssim, psnr])
-    # print(result.min())
-    # print(result.max())
-    # print(result.shape)
-    # result = np.reshape(result, (256, 256, 3))
-    # result = (result + 1) * 127.5
-    # resule = result.astype(np.uint8)
-    # print(result.max())
-    cv2.imwrite('F:\\output.png', result[0])
-    cv2.imwrite('F:\\val.png', val[0][:, :, ::-1])
-    # ssim = tf.image.ssim(tf.constant(val), tf.constant(result[0]), 255)
-    # show_ssim = sess.run(ssim)
-    # print(show_ssim)
+#     result, val, ssim, psnr = sess.run([output, val_batch_data, ssim, psnr])
+#     # print(result.min())
+#     # print(result.max())
+#     # print(result.shape)
+#     # result = np.reshape(result, (256, 256, 3))
+#     # result = (result + 1) * 127.5
+#     # resule = result.astype(np.uint8)
+#     # print(result.max())
+#     cv2.imwrite('F:\\output.png', result[0])
+#     cv2.imwrite('F:\\val.png', val[0][:, :, ::-1])
+#     # ssim = tf.image.ssim(tf.constant(val), tf.constant(result[0]), 255)
+#     # show_ssim = sess.run(ssim)
+#     # print(show_ssim)
 
-    # show_ssim, show_psnr, show_l1, show_l2 = sess.run([ssim, psnr, l1_loss, l2_loss])
-    # print(show_ssim, show_psnr, show_l1, show_l2)
+#     # show_ssim, show_psnr, show_l1, show_l2 = sess.run([ssim, psnr, l1_loss, l2_loss])
+#     # print(show_ssim, show_psnr, show_l1, show_l2)
