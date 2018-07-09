3fc79e7dad796ba8a526fe99cb165bb0e97d2493
fengjiran <fengjiran@foxmail.com>
2018-07-09 15:51:55 +0800

    test

--- Inpainting/patch/celebahq/mask.py
+++ Inpainting/patch/celebahq/mask.py
@@ -9,7 +9,19 @@ def bbox2mask_np(bbox, height, width):
                   pad_width=((top, height - h - top), (left, width - w - left)),
                   mode='constant',
                   constant_values=0)
-    mask = np.expand_dims(mask, 0)
+    # mask = np.expand_dims(mask, 0)
     mask = np.expand_dims(mask, -1)
     mask = np.concatenate((mask, mask, mask), axis=3) * 255
     return mask
+
+
+img_path = 'F:\\Datasets\\celebahq\\img00029978.png'
+hole_size = 128
+image_size = 256
+bbox_np = ((image_size - hole_size) // 2,
+           (image_size - hole_size) // 2,
+           hole_size,
+           hole_size)
+mask = bbox2mask_np(bbox_np, image_size, image_size)
+image = cv2.imread(img_path)
+image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
