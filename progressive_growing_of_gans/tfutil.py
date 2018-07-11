6f447da395130d6a4f01490842ff79859b07160a
fengjiran <fengjiran@foxmail.com>
2018-07-11 20:57:13 +0800

    import

---
+++ progressive_growing_of_gans/tfutil.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+import os
+import sys
+import inspect
+import importlib
+import imp
+import numpy as np
+from collections import OrderedDict
+import tensorflow as tf
