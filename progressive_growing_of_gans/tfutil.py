253c2b240e41687c4adcfd07c4362661dee9fecb
fengjiran <fengjiran@foxmail.com>
2018-07-14 15:33:37 +0800

    init function

--- progressive_growing_of_gans/tfutil.py
+++ progressive_growing_of_gans/tfutil.py
@@ -88,3 +88,22 @@ def create_session(config_dict=dict(), force_as_default=False):
 # Initialize all tf.Variables that have not already been initialized.
 # Equivalent to the following, but more efficient and does not bloat the tf graph:
 # tf.variables_initializer(tf.report_unitialized_variables()).run()
+
+
+def init_uninited_vars(vars=None):
+    if vars is None:
+        vars = tf.global_variables()
+    test_vars = []
+    test_ops = []
+    with tf.control_dependencies(None):  # ignore surrounding control_dependencies
+        for var in vars:
+            assert is_tf_expression(var)
+            try:
+                tf.get_default_graph().get_tensor_by_name(var.name.replace(':0', '/IsVariableInitialized:0'))
+            except KeyError:
+                # Op does not exist => variable may be uninitialized.
+                test_vars.append(var)
+                with absolute_name_scope(var.name.split(':')[0]):
+                    test_ops.append(tf.is_variable_initialized(var))
+    init_vars = [var for var, inited in zip(test_vars, run(test_ops)) if not inited]
+    run([var.initializer for var in init_vars])
