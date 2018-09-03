69a9d1a64c990be57a4b73d9dc15c9d682363b3e
fengjiran <fengjiran@foxmail.com>
2018-09-03 09:38:05 +0800

    import libs as order

--- FCN/utils.py
+++ FCN/utils.py
@@ -1,12 +1,12 @@
 import os
 import sys
+import tarfile
+import zipfile
+import scipy.io
 import tensorflow as tf
 import numpy as np
 import scipy.misc as misc
 from six.moves import urllib
-import tarfile
-import zipfile
-import scipy.io
 
 
 def maybe_download_and_extract(dir_path, url_name, is_tarfile=False, is_zipfile=False):
