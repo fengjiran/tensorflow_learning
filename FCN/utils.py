dc116c54a8b3f953ee18e887d9475545aed2792e
fengjiran <fengjiran@foxmail.com>
2018-09-04 10:53:41 +0800

    bn

--- FCN/utils.py
+++ FCN/utils.py
@@ -126,3 +126,14 @@ def batch_norm(x, n_out, phase_train, scope='bn', decay=0.9, eps=1e-5):
                                 trainable=True)
         batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
         ema = tf.train.ExponentialMovingAverage(decay=decay)
+
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([batch_mean, batch_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        mean, var = tf.cond(phase_train,
+                            mean_var_with_update,
+                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, eps)
+    return normed
