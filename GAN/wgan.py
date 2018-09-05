2721a9534337f3cfbdf8f181b0b5cb36a87dec9a
fengjiran <fengjiran@foxmail.com>
2018-09-05 20:18:31 +0800

    add attri

--- GAN/wgan.py
+++ GAN/wgan.py
@@ -3,3 +3,18 @@ import os
 import time
 import numpy as np
 import tensorflow as tf
+
+
+class WGAN(object):
+    """Construct WGAN class."""
+
+    model_name = 'WGAN'  # name for checkpoint
+
+    def __init__(self, sess, epoch, batch_size, z_dim, dataset_name, checkpoint_dir, result_dir, log_dir):
+        self.sess = sess
+        self.dataset_name = dataset_name
+        self.checkpoint_dir = checkpoint_dir
+        self.result_dir = result_dir
+        self.log_dir = log_dir
+        self.epoch = epoch
+        self.batch_size = batch_size
