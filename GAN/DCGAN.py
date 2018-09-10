9e71646bad96f63b93f5d4b802d3ae813fd1c48f
richard <richard@richarddeMacBook-Pro.local>
2018-09-11 00:20:03 +0800

    bn

--- GAN/DCGAN.py
+++ GAN/DCGAN.py
@@ -18,6 +18,10 @@ class DCGAN(object):
                                 name='layer1')
             x = tf.layers.batch_normalization(x, axis=list(range(len(x.get_shape()) - 1)), name='bn1')
             x = tf.reshape(x, (-1, 256, 8, 8))
-            x = tf.layers.conv2d_transpose(x, 128, (3, 3), strides=(2, 2), padding='same',
+            x = tf.layers.conv2d_transpose(x, 128, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu,
                                            kernel_initializer=tf.keras.initializers.glorot_uniform(),
                                            name='layer2')
+            x = tf.layers.batch_normalization(x, axis=list(range(len(x.get_shape()) - 1)), name='bn2')
+            x = tf.layers.conv2d_transpose(x, 64, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu,
+                                           kernel_initializer=tf.keras.initializers.glorot_uniform(),
+                                           name='layer3')
