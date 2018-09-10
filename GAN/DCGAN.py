37e7827b46bb6582d040a8e973b0888aa948a1bd
richard <richard@richarddeMacBook-Pro.local>
2018-09-10 10:07:30 +0800

    add generator

---
+++ GAN/DCGAN.py
@@ -0,0 +1,15 @@
+from __future__ import division
+import os
+
+import numpy as np
+import tensorflow as tf
+
+
+class DCGAN(object):
+    """Construct dcgan model."""
+
+    def __init__(self):
+        pass
+
+    def generator(self, z, y=None):
+        pass
