868b95a8c2caf989757646419e5858501528e275
richard <richard@richarddeMacBook-Pro.local>
2018-09-10 23:13:42 +0800

    init

--- GAN/DCGAN.py
+++ GAN/DCGAN.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 class DCGAN(object):
     """Construct dcgan model."""
 
-    def __init__(self):
+    def __init__(self, input_height=108, input_width=108, batch_size=16, sample_num=64):
         pass
 
     def generator(self, z, y=None):
