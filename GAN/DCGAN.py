7fe7b74431ffe83d28d3fe7894adab5a63c4c19f
fengjiran <fengjiran@foxmail.com>
2018-09-18 10:49:47 +0800

    generator

--- GAN/DCGAN.py
+++ GAN/DCGAN.py
@@ -8,16 +8,16 @@ import tensorflow as tf
 class DCGAN(object):
     """Construct dcgan model."""
 
-    def __init__(self, input_height=108, input_width=108, batch_size=16, sample_num=64):
+    def __init__(self):
         print('Construct the model.')
 
     def generator(self, z, reuse=None):
         with tf.variable_scope('generator', reuse=reuse):
-            x = tf.layers.dense(z, 256 * 8 * 8, activation=tf.nn.relu,
+            x = tf.layers.dense(z, 1024 * 4 * 4,
                                 kernel_initializer=tf.keras.initializers.glorot_uniform(),
                                 name='layer1')
-            x = tf.layers.batch_normalization(x, axis=list(range(len(x.get_shape()) - 1)), name='bn1')
-            x = tf.reshape(x, (-1, 256, 8, 8))
+            x = tf.reshape(x, (-1, 4, 4, 1024))
+            x = tf.nn.relu(x)
 
             x = tf.layers.conv2d_transpose(x, 128, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu,
                                            kernel_initializer=tf.keras.initializers.glorot_uniform(),
