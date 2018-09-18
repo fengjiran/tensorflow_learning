716999af925d6883044b51f1742e4179f69313fc
fengjiran <fengjiran@foxmail.com>
2018-09-18 11:04:55 +0800

    generator

--- GAN/DCGAN.py
+++ GAN/DCGAN.py
@@ -11,26 +11,38 @@ class DCGAN(object):
     def __init__(self):
         print('Construct the model.')
 
-    def generator(self, z, reuse=None):
+    def generator(self, z, training, reuse=None):
         with tf.variable_scope('generator', reuse=reuse):
             x = tf.layers.dense(z, 1024 * 4 * 4,
                                 kernel_initializer=tf.keras.initializers.glorot_uniform(),
                                 name='layer1')
             x = tf.reshape(x, (-1, 4, 4, 1024))
+            x = tf.layers.batch_normalization(x, axis=list(
+                range(len(x.get_shape()) - 1)), training=training, name='bn1')
             x = tf.nn.relu(x)
 
-            x = tf.layers.conv2d_transpose(x, 128, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu,
+            x = tf.layers.conv2d_transpose(x, 512, (5, 5), strides=(2, 2), padding='same',
                                            kernel_initializer=tf.keras.initializers.glorot_uniform(),
                                            name='layer2')
-            x = tf.layers.batch_normalization(x, axis=list(range(len(x.get_shape()) - 1)), name='bn2')
+            x = tf.layers.batch_normalization(x, axis=list(
+                range(len(x.get_shape()) - 1)), training=training, name='bn2')
+            x = tf.nn.relu(x)
 
-            x = tf.layers.conv2d_transpose(x, 64, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu,
+            x = tf.layers.conv2d_transpose(x, 256, (5, 5), strides=(2, 2), padding='same',
                                            kernel_initializer=tf.keras.initializers.glorot_uniform(),
                                            name='layer3')
-            x = tf.layers.batch_normalization(x, axis=list(range(len(x.get_shape()) - 1)), name='bn3')
+            x = tf.layers.batch_normalization(x, axis=list(
+                range(len(x.get_shape()) - 1)), training=training, name='bn3')
+            x = tf.nn.relu(x)
 
-            x = tf.layers.conv2d_transpose(x, 32, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu,
+            x = tf.layers.conv2d_transpose(x, 128, (5, 5), strides=(2, 2), padding='same',
                                            kernel_initializer=tf.keras.initializers.glorot_uniform(),
                                            name='layer4')
-            x = tf.layers.batch_normalization(x, axis=list(range(len(x.get_shape()) - 1)), name='bn4')
-            #
+            x = tf.layers.batch_normalization(x, axis=list(
+                range(len(x.get_shape()) - 1)), training=training, name='bn4')
+            x = tf.nn.relu(x)
+
+            x = tf.layers.conv2d_transpose(x, 3, (5, 5), strides=(2, 2), padding='same',
+                                           kernel_initializer=tf.keras.initializers.glorot_uniform(),
+                                           name='layer5')
+            outputs = tf.tanh(x, name='outputs')
