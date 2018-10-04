781c89abe38252c0baaf5aa32f92509579d42fa9
fengjiran <fengjiran@foxmail.com>
2018-10-04 15:42:12 +0800

    add init func

--- ResNet/resnet_v1_50.py
+++ ResNet/resnet_v1_50.py
@@ -5,3 +5,12 @@ import tensorflow as tf
 
 fc_initializer = tf.contrib.layers.xavier_initializer
 conv2d_initializer = tf.contrib.layers.xavier_initializer_conv2d
+
+
+class ResNet_v1_50(object):
+    """Construct resnet v1 50."""
+
+    def __init__(self, inputs, num_classes=1000, is_training=True, scope='resnet_v1_50'):
+        self.inputs = inputs
+        self.is_training = is_training
+        self.num_classes = num_classes
