58c5470b433d2aa82a8fb04f7b89344fc06b666a
fengjiran <fengjiran@foxmail.com>
2018-10-09 11:31:24 +0800

    init

---
+++ ResNet/resnet_v1_152.py
@@ -0,0 +1,7 @@
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+fc_initializer = tf.contrib.layers.xavier_initializer
+conv2d_initializer = tf.contrib.layers.xavier_initializer_conv2d
