0ca51c9bf35d6b0558cd64278a4f9dc9684bedfb
fengjiran <fengjiran@foxmail.com>
2018-10-23 00:31:20 +0800

    utils

--- Inpainting/patch/celebahq/utils.py
+++ Inpainting/patch/celebahq/utils.py
@@ -147,3 +147,17 @@ def images_summary(images, name, max_outs):
 def gradients_summary(y, x, norm=tf.abs, name='gradients_y_wrt_x'):
     grad = tf.reduce_mean(norm(tf.gradients(y, x)))
     tf.summary.scalar(name, grad)
+
+
+def instance_norm(x, name="instance_norm"):
+    with tf.variable_scope(name):
+        depth = x.get_shape()[3]
+        scale = tf.get_variable("scale", [depth],
+                                initializer=tf.random_normal_initializer(1.0, 0.02, dtype=tf.float32))
+        offset = tf.get_variable("offset", [depth],
+                                 initializer=tf.constant_initializer(0.0))
+        mean, variance = tf.nn.moments(x, axes=[1, 2], keep_dims=True)
+        epsilon = 1e-5
+        inv = tf.rsqrt(variance + epsilon)
+        normalized = (x - mean) * inv
+        return scale * normalized + offset
