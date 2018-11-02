2df0259e43cfdc5d9ac2a41a58957757ff8387ae
fengjiran <fengjiran@foxmail.com>
2018-11-02 12:00:27 +0800

    test

--- test/test_cv2_inpaint.py
+++ test/test_cv2_inpaint.py
@@ -16,10 +16,33 @@ def bbox2mask_np(bbox, height, width):
                   mode='constant',
                   constant_values=0)
     # mask = np.expand_dims(mask, 0)
-    # mask = np.expand_dims(mask, -1)
+    mask = np.expand_dims(mask, -1)
     # mask = np.concatenate((mask, mask, mask), axis=3) * 255
     return mask
 
 
 if __name__ == '__main__':
     img = cv.imread('val.png')
+    height = img.shape[0]
+    width = img.shape[1]
+
+    bbox = random_bbox(height, width, 128, 128)
+    mask = bbox2mask_np(bbox, height, width)
+
+    img = img / 127.5 - 1
+
+    destroy = (img + 1.) * 0.5 * (1. - mask)
+    destroy = destroy * 255.
+    destroy = destroy.astype(np.uint8)
+
+    # src = img * (1.0 - mask) * 255.
+    dst = cv.inpaint(destroy, mask.astype(np.uint8), 5, cv.INPAINT_TELEA)
+    dst = dst.astype(np.uint8)
+    print(dst[0][0][2])
+
+    cv.imshow('img', (img + 1.) * 0.5)
+    cv.imshow('src', destroy)
+    cv.imshow('dst', dst)
+    cv.waitKey(0)
+
+    # dst = cv.inpaint()
