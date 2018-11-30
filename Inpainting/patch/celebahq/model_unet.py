dbc95e2bd7d70897e8d04f15e6ba743730e65ef7
fengjiran <fengjiran@foxmail.com>
2018-11-30 11:30:57 +0800

    build wgan dis

--- Inpainting/patch/celebahq/model_unet.py
+++ Inpainting/patch/celebahq/model_unet.py
@@ -317,6 +317,23 @@ class CompletionModel(object):
                                            name='conv4')(conv3)
             return tf.reduce_mean(conv4, axis=[1, 2, 3])
 
+    def build_wgan_discriminator(self, global_input, local_input, reuse=None):
+        with tf.variable_scope('wgan_discriminator', reuse=reuse):
+            dglobal = self.global_discriminator(global_input, reuse=reuse)
+            dlocal = self.local_discriminator(local_input, reuse=reuse)
+
+            dout_global = tf.keras.layers.Dense(units=1,
+                                                name='dout_global_fc')(dglobal)
+
+            # dout_global = tf.layers.dense(dglobal, 1, kernel_initializer=self.fc_init,
+            #                               name='dout_global_fc')
+            dout_local = dlocal
+            # dout_local = tf.layers.dense(dlocal, 1, name='dout_local_fc')
+            # dout_local = tf.layers.dense(dlocal, 256, name='dout_local_fc')
+            # dout_local = tf.reduce_mean(dout_local, axis=1)
+
+            return dout_global, dout_local
+
 
 if __name__ == '__main__':
     model = CompletionModel()
