be038e367fdddb8650100c7f5057452cfe738e1d
fengjiran <fengjiran@foxmail.com>
2018-11-30 11:39:09 +0800

    change the batch size

--- Inpainting/patch/celebahq/model_in.py
+++ Inpainting/patch/celebahq/model_in.py
@@ -521,7 +521,7 @@ if __name__ == '__main__':
         cfg = yaml.load(f)
 
     model = CompletionModel()
-    x = tf.random_uniform([10, 256, 256, 3])
+    x = tf.random_uniform([cfg['batch_size'], 256, 256, 3])
 
     # coarse = model.coarse_network(x)
     # refine = model.refine_network(coarse)
