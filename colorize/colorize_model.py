c8b1b2abc1f4b2ba755ca157fb54c0755aef56fe
fengjiran <fengjiran@foxmail.com>
2018-12-10 11:17:50 +0800

    add networks

--- colorize/colorize_model.py
+++ colorize/colorize_model.py
@@ -2,3 +2,20 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+
+
+class Colorize(object):
+    def __init__(self):
+        pass
+
+    def low_level_network(self, inputs):
+        pass
+
+    def mid_level_network(self, inputs):
+        pass
+
+    def global_level_network(self, inputs):
+        pass
+
+    def colorize_network(self, inputs):
+        pass
