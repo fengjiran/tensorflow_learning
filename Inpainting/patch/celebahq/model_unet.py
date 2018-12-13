1ad027a76b66bb281e5f717dbe975814b0e39f57
fengjiran <fengjiran@foxmail.com>
2018-12-13 10:07:33 +0800

    flatten

--- Inpainting/patch/celebahq/model_unet.py
+++ Inpainting/patch/celebahq/model_unet.py
@@ -282,14 +282,14 @@ class CompletionModel(object):
                                                       kernel_initializer=self.conv_init,
                                                       name='conv4'))
 
-            conv5 = tf.layers.conv2d(conv4, 1, 1,
-                                     padding='same',
-                                     kernel_initializer=self.conv_init,
-                                     name='conv5')
-            # fc1 = tf.contrib.layers.flatten(conv4)
-            # fc2 = tf.layers.dense(fc1, 1, kernel_initializer=self.fc_init,
-            #                       name='dout_global_fc')
-            return conv5
+            # conv5 = tf.layers.conv2d(conv4, 1, 1,
+            #                          padding='same',
+            #                          kernel_initializer=self.conv_init,
+            #                          name='conv5')
+            fc1 = tf.layers.flatten(conv4)
+            fc2 = tf.layers.dense(fc1, 1, kernel_initializer=self.fc_init,
+                                  name='dout_global_fc')
+            return fc2
 
     def local_discriminator(self, x, reuse=None):
         cnum = 64
