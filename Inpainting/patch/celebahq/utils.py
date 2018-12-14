4c26ee6f92e6d57d95a2989ac4ae6204bbf3521b
fengjiran <fengjiran@foxmail.com>
2018-12-14 15:17:17 +0800

    check image

--- Inpainting/patch/celebahq/utils.py
+++ Inpainting/patch/celebahq/utils.py
@@ -11,6 +11,15 @@ def check_image(image):
     with tf.control_dependencies([assertion]):
         image = tf.identity(image)
 
+    if image.get_shape().ndims not in (3, 4):
+        raise ValueError('image must be either 3 or 4 dimentions')
+
+    # make the last dimension 3 so that you can unstack the colors
+    shape = list(image.get_shape())
+    shape[-1] = 3
+    image.set_shape(shape)
+    return image
+
 
 def deprocess(image):
     # [-1, 1] => [0, 1]
