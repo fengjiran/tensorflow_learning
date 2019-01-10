818bbd2d85aef2aee503c74f612337ce48ecf6b3
fengjiran <fengjiran@foxmail.com>
2019-01-10 22:07:43 +0800

    ops

--- Inpainting/EdgeConnect/ops.py
+++ Inpainting/EdgeConnect/ops.py
@@ -2,3 +2,60 @@ import tensorflow as tf
 
 weight_init = tf.truncated_normal_initializer(mean=0.0, stddev=0.02)
 weight_regularizer = None
+
+
+def conv(x, channels, kernel=4, stride=2, pad=0, pad_type='zero', use_bias=True, sn=False, scope='conv_0'):
+    with tf.variable_scope(scope):
+        if pad_type == 'zero':
+            x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]])
+        if pad_type == 'reflect':
+            x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]], mode='REFLECT')
+
+        if sn:
+            w = tf.get_variable("kernel", shape=[kernel, kernel, x.get_shape()[-1], channels],
+                                initializer=weight_init,
+                                regularizer=weight_regularizer)
+            bias = tf.get_variable("bias", [channels], initializer=tf.constant_initializer(0.0))
+            x = tf.nn.conv2d(input=x, filter=spectral_norm(w),
+                             strides=[1, stride, stride, 1], padding='VALID')
+
+            if use_bias:
+                x = tf.nn.bias_add(x, bias)
+
+        else:
+            x = tf.layers.conv2d(inputs=x, filters=channels,
+                                 kernel_size=kernel, kernel_initializer=weight_init,
+                                 kernel_regularizer=weight_regularizer,
+                                 strides=stride, use_bias=use_bias)
+
+        return x
+
+
+def spectral_norm(w, iteration=1):
+    w_shape = w.shape().as_list()
+    w = tf.reshape(w, [-1, w_shape[-1]])
+
+    u = tf.get_variable('u', [1, w_shape[-1]],
+                        initializer=tf.random_normal_initializer(),
+                        trainable=False)
+
+    u_hat = u
+    v_hat = None
+
+    for i in range(iteration):
+        v_ = tf.matmul(u_hat, tf.transpose(w))
+        v_hat = tf.nn.l2_normalize(v_)
+
+        u_ = tf.matmul(v_hat, w)
+        u_hat = tf.nn.l2_normalize(u_)
+
+    u_hat = tf.stop_gradient(u_hat)
+    v_hat = tf.stop_gradient(v_hat)
+
+    sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
+
+    with tf.control_dependencies([u.assign(u_hat)]):
+        w_norm = w / sigma
+        w_norm = tf.reshape(w_norm, w_shape)
+
+    return w_norm
