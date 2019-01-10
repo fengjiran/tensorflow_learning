50e9d6a214d4caaebdb0b3853c8079035d19168e
fengjiran <fengjiran@foxmail.com>
2019-01-10 15:33:14 +0800

    add spectral norm

--- Inpainting/EdgeConnect/networks.py
+++ Inpainting/EdgeConnect/networks.py
@@ -14,3 +14,33 @@ def instance_norm(x, name="instance_norm"):
         inv = tf.rsqrt(variance + epsilon)
         normalized = (x - mean) * inv
         return scale * normalized + offset
+
+
+def spectral_norm(w, iteration=1):
+    w_shape = w.shape().as_list()
+    w = tf.reshape(w, [-1, w_shape[-1]])
+
+    u = tf.get_variable('u', [1, w_shape[-1]],
+                        initializer=tf.random_normal_initializer(),
+                        trainable=False)
+
+    u_hat = u
+    v_hat = None
+
+    for i in range(iteration):
+        v_ = tf.matmul(u_hat, tf.transpose(w))
+        v_hat = tf.nn.l2_normalize(v_)
+
+        u_ = tf.matmul(v_hat, w)
+        u_hat = tf.nn.l2_normalize(u_)
+
+    u_hat = tf.stop_gradient(u_hat)
+    v_hat = tf.stop_gradient(v_hat)
+
+    sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
+
+    with tf.control_dependencies([u.assign(u_hat)]):
+        w_norm = w / sigma
+        w_norm = tf.reshape(w_norm, w_shape)
+
+    return w_norm
