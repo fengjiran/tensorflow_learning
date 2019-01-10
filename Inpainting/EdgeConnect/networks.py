c18685423d00e37c86864f3c0cecebf4f362d18c
fengjiran <fengjiran@foxmail.com>
2019-01-10 15:24:00 +0800

    add instance norm layer

--- Inpainting/EdgeConnect/networks.py
+++ Inpainting/EdgeConnect/networks.py
@@ -1,2 +1,16 @@
 from __future__ import print_function
 import tensorflow as tf
+
+
+def instance_norm(x, name="instance_norm"):
+    with tf.variable_scope(name):
+        depth = x.get_shape()[3]
+        scale = tf.get_variable("scale", [depth],
+                                initializer=tf.random_normal_initializer(1.0, 0.02, dtype=tf.float32))
+        offset = tf.get_variable("offset", [depth],
+                                 initializer=tf.constant_initializer(0.0))
+        mean, variance = tf.nn.moments(x, axes=[1, 2], keep_dims=True)
+        epsilon = 1e-5
+        inv = tf.rsqrt(variance + epsilon)
+        normalized = (x - mean) * inv
+        return scale * normalized + offset
