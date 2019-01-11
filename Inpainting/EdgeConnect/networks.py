fb7cf9b967ca7fa1b1d330f15410e63cbcc83198
fengjiran <fengjiran@foxmail.com>
2019-01-11 10:25:52 +0800

    networks

--- Inpainting/EdgeConnect/networks.py
+++ Inpainting/EdgeConnect/networks.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 
 from ops import conv
 from ops import deconv
+from ops import resnet_block
+from ops import instance_norm
 
 
 class InpaintingModel(object):
@@ -16,17 +18,3 @@ class InpaintingModel(object):
 
     def inpaint_generator(self, x):
         pass
-
-
-def instance_norm(x, name="instance_norm"):
-    with tf.variable_scope(name):
-        depth = x.get_shape()[3]
-        scale = tf.get_variable("scale", [depth],
-                                initializer=tf.random_normal_initializer(1.0, 0.02, dtype=tf.float32))
-        offset = tf.get_variable("offset", [depth],
-                                 initializer=tf.constant_initializer(0.0))
-        mean, variance = tf.nn.moments(x, axes=[1, 2], keep_dims=True)
-        epsilon = 1e-5
-        inv = tf.rsqrt(variance + epsilon)
-        normalized = (x - mean) * inv
-        return scale * normalized + offset
