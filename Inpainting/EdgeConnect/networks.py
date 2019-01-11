40f2f7805c7541d6e2f810d1fabed0026d762cff
fengjiran <fengjiran@foxmail.com>
2019-01-11 11:33:51 +0800

    edge generator

--- Inpainting/EdgeConnect/networks.py
+++ Inpainting/EdgeConnect/networks.py
@@ -14,7 +14,21 @@ class InpaintingModel(object):
         print('Construct the inpainting model.')
 
     def edge_generator(self, x):
-        pass
+        with tf.variable_scope('edge_generator'):
+            # encoder
+            x = conv(x, channels=64, kernel=7, stride=1, pad=3, pad_type='reflect', name='conv1')
+            x = instance_norm(x, name='in1')
+            x = tf.nn.relu(x)
+
+            x = conv(x, channels=128, kernel=4, stride=2, pad=1, pad_type='zero', name='conv2')
+            x = instance_norm(x, name='in2')
+            x = tf.nn.relu(x)
+
+            x = conv(x, channels=256, kernel=4, stride=2, pad=1, pad_type='zero', name='conv3')
+            x = instance_norm(x, name='in3')
+            x = tf.nn.relu(x)
+
+            return x
 
     def inpaint_generator(self, x):
         pass
