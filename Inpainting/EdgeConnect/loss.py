ec3f97e661fb8c5094a12c6f533a5e91973e6144
fengjiran <fengjiran@foxmail.com>
2019-01-12 12:09:06 +0800

    add loss

--- Inpainting/EdgeConnect/loss.py
+++ Inpainting/EdgeConnect/loss.py
@@ -18,6 +18,7 @@ class Vgg19(object):
             print(vgg19_npy_path)
 
         self.data_dict = np.load(vgg19_npy_path, encoding='latin1').item()
+        print('npy file loaded')
 
     def avg_pool(self, bottom, name):
         return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
@@ -27,4 +28,21 @@ class Vgg19(object):
 
     def conv_layer(self, bottom, name):
         with tf.variable_scope(name):
-            pass
+            filt = self.get_conv_filter(name)
+
+            conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
+            conv_biases = self.get_bias(name)
+
+            bias = tf.nn.bias_add(conv, conv_biases)
+
+            relu = tf.nn.relu(bias)
+            return relu
+
+    def get_conv_filter(self, name):
+        return tf.constant(self.data_dict[name][0], name='filters')
+
+    def get_bias(self, name):
+        return tf.constant(self.data_dict[name][1], name='biases')
+
+    def get_fc_weight(self, name):
+        return tf.constant(self.data_dict[name][0], name='weights')
