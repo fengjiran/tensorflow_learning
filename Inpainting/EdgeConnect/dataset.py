4110c6c4e7741499b2e7041a62ac82fb92657e49
fengjiran <fengjiran@foxmail.com>
2019-01-15 16:49:54 +0800

    add Dataset class

--- Inpainting/EdgeConnect/dataset.py
+++ Inpainting/EdgeConnect/dataset.py
@@ -1,2 +1,10 @@
 import os
 import numpy as np
+
+
+class Dataset():
+    """Construct dataset class."""
+
+    def __init__(self, config, flist, edge_flist, mask_flist, augment=True, training=True):
+        self.augment = augment
+        self.training = training
