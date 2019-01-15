f1737474298522b3a172bb46263e4566a214b204
fengjiran <fengjiran@foxmail.com>
2019-01-15 15:51:08 +0800

    update

--- Inpainting/EdgeConnect/edge_connect.py
+++ Inpainting/EdgeConnect/edge_connect.py
@@ -9,3 +9,18 @@ class EdgeConnenct():
 
     def __init__(self, config=None):
         self.cfg = config
+
+        if self.cfg['MODEL'] == 1:
+            model_name = 'edge'
+        elif self.cfg['MODEL'] == 2:
+            model_name = 'inpaint'
+        elif self.cfg['MODEL'] == 3:
+            model_name = 'edge_inpaint'
+        elif self.cfg['MODEL'] == 4:
+            model_name = 'joint'
+
+    def train(self):
+        epoch = 0
+        keep_training = True
+        model = self.cfg['MODEL']
+        max_iteration = int(self.cfg['MAX_ITERS'])
