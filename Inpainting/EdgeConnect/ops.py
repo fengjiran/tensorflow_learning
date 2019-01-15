0d43c819a24398c5f6a60cace07cb87f0e2e835b
fengjiran <fengjiran@foxmail.com>
2019-01-15 12:31:25 +0800

    use sn

--- Inpainting/EdgeConnect/ops.py
+++ Inpainting/EdgeConnect/ops.py
@@ -1,12 +1,21 @@
 import tensorflow as tf
 
-weight_init = tf.truncated_normal_initializer(mean=0.0, stddev=0.02)
-weight_regularizer = None
+# weight_init = tf.truncated_normal_initializer(mean=0.0, stddev=0.02)
+# weight_regularizer = None
 
 
 def conv(x, channels, kernel=4, stride=1, dilation=1,
-         pad=0, pad_type='zero', use_bias=True, sn=True, name='conv_0'):
+         pad=0, pad_type='zero', use_bias=True, sn=True, init_type='normal', name='conv_0'):
     with tf.variable_scope(name):
+        if init_type == 'normal':
+            weight_init = tf.truncated_normal_initializer(mean=0.0, stddev=0.02)
+        elif init_type == 'xavier':
+            weight_init = tf.glorot_normal_initializer()
+        elif init_type == 'kaiming':
+            weight_init = tf.keras.initializers.he_normal()
+        elif init_type == 'orthogonal':
+            weight_init = tf.orthogonal_initializer(gain=0.02)
+
         if pad_type == 'zero':
             x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]])
         if pad_type == 'reflect':
@@ -15,7 +24,7 @@ def conv(x, channels, kernel=4, stride=1, dilation=1,
         if sn:
             w = tf.get_variable("kernel", shape=[kernel, kernel, x.get_shape()[-1], channels],
                                 initializer=weight_init,
-                                regularizer=weight_regularizer)
+                                regularizer=None)
             bias = tf.get_variable("bias", [channels], initializer=tf.constant_initializer(0.0))
             x = tf.nn.conv2d(input=x,
                              filter=spectral_norm(w),
@@ -29,21 +38,30 @@ def conv(x, channels, kernel=4, stride=1, dilation=1,
         else:
             x = tf.layers.conv2d(inputs=x, filters=channels,
                                  kernel_size=kernel, kernel_initializer=weight_init,
-                                 kernel_regularizer=weight_regularizer,
+                                 kernel_regularizer=None,
                                  strides=stride, use_bias=use_bias)
 
         return x
 
 
-def deconv(x, channels, kernel=4, stride=1, use_bias=True, sn=True, name='deconv_0'):
+def deconv(x, channels, kernel=4, stride=1, use_bias=True, sn=True, init_type='normal', name='deconv_0'):
     with tf.variable_scope(name):
+        if init_type == 'normal':
+            weight_init = tf.truncated_normal_initializer(mean=0.0, stddev=0.02)
+        elif init_type == 'xavier':
+            weight_init = tf.glorot_normal_initializer()
+        elif init_type == 'kaiming':
+            weight_init = tf.keras.initializers.he_normal()
+        elif init_type == 'orthogonal':
+            weight_init = tf.orthogonal_initializer(gain=0.02)
+
         x_shape = x.get_shape().as_list()
         output_shape = [x_shape[0], x_shape[1] * stride, x_shape[2] * stride, channels]
 
         if sn:
             w = tf.get_variable("kernel", shape=[kernel, kernel, channels, x.get_shape()[-1]],
                                 initializer=weight_init,
-                                regularizer=weight_regularizer)
+                                regularizer=None)
             x = tf.nn.conv2d_transpose(x, filter=spectral_norm(w),
                                        output_shape=output_shape,
                                        strides=[1, stride, stride, 1],
@@ -56,7 +74,7 @@ def deconv(x, channels, kernel=4, stride=1, use_bias=True, sn=True, name='deconv
             x = tf.layers.conv2d_transpose(inputs=x, filters=channels,
                                            kernel_size=kernel,
                                            kernel_initializer=weight_init,
-                                           kernel_regularizer=weight_regularizer,
+                                           kernel_regularizer=None,
                                            strides=stride,
                                            padding='SAME',
                                            use_bias=use_bias)
@@ -64,12 +82,21 @@ def deconv(x, channels, kernel=4, stride=1, use_bias=True, sn=True, name='deconv
         return x
 
 
-def atrous_conv(x, channels, kernel=3, dilation=1, use_bias=True, sn=True, name='conv_0'):
+def atrous_conv(x, channels, kernel=3, dilation=1, use_bias=True, sn=True, init_type='normal', name='conv_0'):
     with tf.variable_scope(name):
+        if init_type == 'normal':
+            weight_init = tf.truncated_normal_initializer(mean=0.0, stddev=0.02)
+        elif init_type == 'xavier':
+            weight_init = tf.glorot_normal_initializer()
+        elif init_type == 'kaiming':
+            weight_init = tf.keras.initializers.he_normal()
+        elif init_type == 'orthogonal':
+            weight_init = tf.orthogonal_initializer(gain=0.02)
+
         if sn:
             w = tf.get_variable("kernel", shape=[kernel, kernel, x.get_shape()[-1], channels],
                                 initializer=weight_init,
-                                regularizer=weight_regularizer)
+                                regularizer=None)
             bias = tf.get_variable("bias", [channels], initializer=tf.constant_initializer(0.0))
 
             x = tf.nn.atrous_conv2d(value=x,
@@ -82,22 +109,23 @@ def atrous_conv(x, channels, kernel=3, dilation=1, use_bias=True, sn=True, name=
         else:
             x = tf.layers.conv2d(inputs=x, filters=channels,
                                  kernel_size=kernel, kernel_initializer=weight_init,
-                                 kernel_regularizer=weight_regularizer,
+                                 kernel_regularizer=None,
                                  use_bias=use_bias, dilation_rate=dilation)
 
         return x
 
 
-def resnet_block(x, out_channels, dilation=1, name='resnet_block'):
+def resnet_block(x, out_channels, dilation=1, init_type='normal', sn=True, name='resnet_block'):
     with tf.variable_scope(name):
-        y = atrous_conv(x, out_channels, kernel=3, dilation=dilation, name='conv1')
+        y = atrous_conv(x, out_channels, kernel=3, dilation=dilation, sn=sn,
+                        init_type=init_type, name='conv1')
         # y = conv(x, out_channels, kernel=3, stride=1, dilation=dilation,
         #          pad=dilation, pad_type='reflect', name='conv1')
         y = instance_norm(y, name='in1')
         y = tf.nn.relu(y)
 
-        y = conv(y, out_channels, kernel=3, stride=1, dilation=1,
-                 pad=1, pad_type='reflect', name='conv2')
+        y = conv(y, out_channels, kernel=3, stride=1, dilation=1, sn=sn,
+                 pad=1, pad_type='reflect', init_type=init_type, name='conv2')
         y = instance_norm(y, name='in2')
 
         return x + y
