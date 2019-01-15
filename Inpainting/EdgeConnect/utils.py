f6a2222c307c9894afd1e51c706cb75b32d1ad36
fengjiran <fengjiran@foxmail.com>
2019-01-15 16:49:44 +0800

    uodate

--- Inpainting/EdgeConnect/utils.py
+++ Inpainting/EdgeConnect/utils.py
@@ -10,3 +10,28 @@ from PIL import Image
 def create_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+def create_mask(height, width, mask_height, mask_width, x=None, y=None):
+    mask = np.zeros([height, width])
+    mask_x = x if x is not None else random.randint(0, width - mask_width)
+    mask_y = y if y is not None else random.randint(0, height - mask_height)
+    mask[mask_y:mask_y + mask_height, mask_x:mask_x + mask_width] = 1.
+    return mask
+
+
+def stitch_images(inputs, *outputs, img_per_row=2):
+    gap = 5
+    columns = len(outputs) + 1
+
+    width, height = inputs[0][:, :, 0].shape
+    img = Image.new('RGB', (width * img_per_row * columns + gap *
+                            (img_per_row - 1), height * int(len(inputs) / img_per_row)))
+    images = [inputs, *outputs]
+
+    for ix in range(len(inputs)):
+        xoffset = int(ix % img_per_row) * width * columns + int(ix % img_per_row) * gap
+        yoffset = int(ix / img_per_row) * height
+
+        for cat in range(len(images)):
+            pass
