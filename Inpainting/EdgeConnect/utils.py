8187d100caa2eb0724db58a878ef35a23c476cfe
fengjiran <fengjiran@foxmail.com>
2019-01-18 10:39:04 +0800

    create mask

--- Inpainting/EdgeConnect/utils.py
+++ Inpainting/EdgeConnect/utils.py
@@ -14,13 +14,13 @@ def create_dir(path):
 
 
 def create_mask(height, width, mask_height, mask_width, x=None, y=None):
-    top = tf.random_uniform([], minval=0, maxval=height - mask_height, dtype=tf.int32)
-    left = tf.random_uniform([], minval=0, maxval=width - mask_width, dtype=tf.int32)
+    top = y if y is not None else tf.random_uniform([], minval=0, maxval=height - mask_height, dtype=tf.int32)
+    left = x if x is not None else tf.random_uniform([], minval=0, maxval=width - mask_width, dtype=tf.int32)
 
-    mask = np.zeros([height, width])
-    mask_x = x if x is not None else random.randint(0, width - mask_width)
-    mask_y = y if y is not None else random.randint(0, height - mask_height)
-    mask[mask_y:mask_y + mask_height, mask_x:mask_x + mask_width] = 1.
+    mask = tf.pad(tensor=tf.ones((mask_height, mask_width), dtype=tf.float32),
+                  paddings=[[top, height - mask_height - top],
+                            [left, width - mask_width - left]])
+
     return mask
 
 
