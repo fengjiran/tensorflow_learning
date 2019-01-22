89cfa460e95218e4935dd232693dbf3cd8debbd2
fengjiran <fengjiran@foxmail.com>
2019-01-22 17:11:19 +0800

    add dis func

--- Inpainting/two_stage/celebahq/networks.py
+++ Inpainting/two_stage/celebahq/networks.py
@@ -61,3 +61,30 @@ class InpaintingModel():
             x = tf.nn.tanh(x)
 
             return x
+
+    def coarse_discriminator(self, x, reuse=None, use_sigmoid=False):
+        with tf.variable_scope('coarse_discriminator', reuse=reuse):
+            conv1 = conv(x, channels=64, kernel=4, stride=2, pad=1, pad_type='zero',
+                         use_bias=False, init_type=self.init_type, name='conv1')
+            conv1 = tf.nn.leaky_relu(conv1)
+
+            conv2 = conv(conv1, channels=128, kernel=4, stride=2, pad=1, pad_type='zero',
+                         use_bias=False, init_type=self.init_type, name='conv2')
+            conv2 = tf.nn.leaky_relu(conv2)
+
+            conv3 = conv(conv2, channels=256, kernel=4, stride=2, pad=1, pad_type='zero',
+                         use_bias=False, init_type=self.init_type, name='conv3')
+            conv3 = tf.nn.leaky_relu(conv3)
+
+            conv4 = conv(conv3, channels=512, kernel=4, stride=1, pad=1, pad_type='zero',
+                         use_bias=False, init_type=self.init_type, name='conv4')
+            conv4 = tf.nn.leaky_relu(conv4)
+
+            conv5 = conv(conv4, channels=1, kernel=4, stride=1, pad=1, pad_type='zero',
+                         use_bias=False, init_type=self.init_type, name='conv5')
+
+            outputs = conv5
+            if use_sigmoid:
+                outputs = tf.nn.sigmoid(conv5)
+
+            return outputs, [conv1, conv2, conv3, conv4, conv5]
