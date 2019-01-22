1d90481c473f2b0145d3513130047c12c4cad697
fengjiran <fengjiran@foxmail.com>
2019-01-22 17:02:22 +0800

    add model

--- Inpainting/two_stage/celebahq/networks.py
+++ Inpainting/two_stage/celebahq/networks.py
@@ -9,3 +9,11 @@ from ops import instance_norm
 from loss import adversarial_loss
 from loss import perceptual_loss
 from loss import style_loss
+
+
+class InpaintingModel():
+    """Construct model."""
+
+    def __init__(self, config=None):
+        print('Construct the inpainting model.')
+        self.cfg = config
