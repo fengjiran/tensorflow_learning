85bb517f45f13a71de838303f2249617a53444be
fengjiran <fengjiran@foxmail.com>
2019-01-23 14:27:56 +0800

    add data

--- Inpainting/two_stage/celebahq/coarse_refine.py
+++ Inpainting/two_stage/celebahq/coarse_refine.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 from .networks import InpaintingModel
+from.dataset import Dataset
 
 
 class CoarseRefine():
@@ -10,6 +11,7 @@ class CoarseRefine():
     def __init__(self, config):
         self.cfg = config
         self.model = InpaintingModel(config)
+        self.train_dataset = Dataset(config)
 
     def train(self):
         pass
