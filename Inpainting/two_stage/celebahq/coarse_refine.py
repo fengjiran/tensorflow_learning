3fdb0b867600855fa8c10fa3d25cd2138682dfa4
fengjiran <fengjiran@foxmail.com>
2019-01-23 10:38:26 +0800

    add train

--- Inpainting/two_stage/celebahq/coarse_refine.py
+++ Inpainting/two_stage/celebahq/coarse_refine.py
@@ -2,3 +2,14 @@ import os
 import numpy as np
 import tensorflow as tf
 from .networks import InpaintingModel
+
+
+class CoarseRefine():
+    """Construct model."""
+
+    def __init__(self, config):
+        self.cfg = config
+        self.model = InpaintingModel(config)
+
+    def train(self):
+        pass
