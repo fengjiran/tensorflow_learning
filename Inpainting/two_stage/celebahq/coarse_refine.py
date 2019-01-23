2a26414e32e69c6b63db8199fde01f27b5358574
fengjiran <fengjiran@foxmail.com>
2019-01-23 16:39:54 +0800

    summary

--- Inpainting/two_stage/celebahq/coarse_refine.py
+++ Inpainting/two_stage/celebahq/coarse_refine.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 from .networks import InpaintingModel
-from.dataset import Dataset
+from .dataset import Dataset
 
 
 class CoarseRefine():
@@ -12,8 +12,11 @@ class CoarseRefine():
         self.cfg = config
         self.model = InpaintingModel(config)
         self.train_dataset = Dataset(config)
-        images, masks = self.train_dataset.load_item()
+
+    def train(self):
+        images, masks, train_iterator = self.train_dataset.load_item()
         flist = self.train_dataset.load_flist(self.cfg['FLIST'])
+        num_batch = len(flist) // self.cfg['BATCH_SIZE']
 
         coarse_outputs, coarse_outputs_merged, coarse_gen_loss, coarse_dis_loss, coarse_gen_train, coarse_dis_train =\
             self.model.build_coarse_model(images, masks)
@@ -23,5 +26,22 @@ class CoarseRefine():
             coarse_dis_train_ops.append(coarse_dis_train)
         coarse_dis_train = tf.group(*coarse_dis_train_ops)
 
-    def train(self):
-        pass
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        with tf.Session(config=config) as sess:
+            sess.run(train_iterator.initializer, feed_dict={self.train_dataset.train_filenames: flist})
+
+            step = 0
+            while True:
+                _, _, gen_loss, dis_loss = sess.run([coarse_gen_train,
+                                                     coarse_dis_loss,
+                                                     coarse_gen_loss,
+                                                     coarse_dis_loss])
+                print('Epoch: {}, Iter: {}, coarse_gen_loss: {}, coarse_dis_loss: {}'.format(
+                    step // num_batch + 1,
+                    step,
+                    gen_loss,
+                    dis_loss
+                ))
+
+                step += 1
