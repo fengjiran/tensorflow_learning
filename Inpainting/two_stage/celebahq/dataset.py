2050374fe3ae1d5731470f9f857b25a94e497802
fengjiran <fengjiran@foxmail.com>
2019-01-23 11:44:06 +0800

    dataset

--- Inpainting/two_stage/celebahq/dataset.py
+++ Inpainting/two_stage/celebahq/dataset.py
@@ -1,4 +1,38 @@
 import os
 import glob
+import numpy as np
 
 from .utils import create_mask
+
+
+class Dataset():
+    """Construct dataset class."""
+
+    def __init__(self, config, training=True):
+        self.cfg = config
+        self.training = training
+        self.flist = self.cfg['FLIST']
+
+    def load_item(self):
+        pass
+
+    def load_flist(self, flist):
+        if isinstance(flist, list):
+            return flist
+
+        # flist: image file path, image directory path, text file flist path
+        if isinstance(flist, str):
+            if os.path.isdir(flist):
+                flist = list(glob.glob(flist + '/*.jpg')) + list(glob.glob(flist + '/*.png')) + \
+                    list(glob.glob(flist + '/*.JPG'))
+                flist.sort()
+                return flist
+
+            if os.path.isfile(flist):
+                try:
+                    print('is a file')
+                    return np.genfromtxt(flist, dtype=np.str, encoding='utf-8')
+                except:
+                    return [flist]
+
+        return []
