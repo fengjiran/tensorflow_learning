d54786f57ffc25c91717211fe5c805742f8a0c2b
fengjiran <fengjiran@foxmail.com>
2019-01-23 14:49:38 +0800

    add dis train

--- Inpainting/two_stage/celebahq/coarse_refine.py
+++ Inpainting/two_stage/celebahq/coarse_refine.py
@@ -12,6 +12,16 @@ class CoarseRefine():
         self.cfg = config
         self.model = InpaintingModel(config)
         self.train_dataset = Dataset(config)
+        images, masks = self.train_dataset.load_item()
+        flist = self.train_dataset.load_flist(self.cfg['FLIST'])
+
+        coarse_outputs, coarse_outputs_merged, coarse_gen_loss, coarse_dis_loss, coarse_gen_train, coarse_dis_train =\
+            self.model.build_coarse_model(images, masks)
+
+        coarse_dis_train_ops = []
+        for i in range(5):
+            coarse_dis_train_ops.append(coarse_dis_train)
+        coarse_dis_train = tf.group(*coarse_dis_train_ops)
 
     def train(self):
         pass
