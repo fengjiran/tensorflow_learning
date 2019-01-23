d24b325dd53bbaae197b523743ba5d51ad1df4b6
fengjiran <fengjiran@foxmail.com>
2019-01-23 14:03:06 +0800

    return images

--- Inpainting/two_stage/celebahq/dataset.py
+++ Inpainting/two_stage/celebahq/dataset.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import numpy as np
+import tensorflow as tf
 
 from .utils import create_mask
 
@@ -15,7 +16,24 @@ class Dataset():
         self.train_filenames = tf.placeholder(tf.string, shape=[None])
 
     def load_item(self):
-        pass
+        train_dataset = tf.data.Dataset.from_tensor_slices(self.train_filenames)
+        train_dataset = train_dataset.map(self.input_parse)
+        train_dataset = train_dataset.shuffle(buffer_size=2000)
+        train_dataset = train_dataset.apply(tf.contrib.data.batch_and_drop_remainder(self.cfg['BATCH_SIZE']))
+        train_dataset = train_dataset.repeat()
+        train_iterator = train_dataset.make_initializable_iterator()
+        images = train_iterator.get_next()
+
+        return images
+
+    def input_parse(self, img_path):
+        with tf.device('/cpu:0'):
+            img_file = tf.read_file(img_path)
+            img_decoded = tf.image.decode_jpeg(img_file, channels=3)
+            img = tf.cast(img_decoded, tf.float32)
+            img = tf.image.resize_image_with_crop_or_pad(img, self.cfg['INPUT_SIZE'], self.cfg['INPUT_SIZE'])
+            img = img / 127.5 - 1
+            return img  # [-1, 1]
 
     def load_flist(self, flist):
         if isinstance(flist, list):
