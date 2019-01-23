0a0f1b95678371d21ee407b0a9ae8f8931be63be
fengjiran <fengjiran@foxmail.com>
2019-01-23 11:13:40 +0800

    create mask

---
+++ Inpainting/two_stage/celebahq/utils.py
@@ -0,0 +1,19 @@
+import os
+
+import tensorflow as tf
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def create_mask(height, width, mask_height, mask_width, x=None, y=None):
+    top = y if y is not None else tf.random_uniform([], minval=0, maxval=height - mask_height, dtype=tf.int32)
+    left = x if x is not None else tf.random_uniform([], minval=0, maxval=width - mask_width, dtype=tf.int32)
+
+    mask = tf.pad(tensor=tf.ones((mask_height, mask_width), dtype=tf.float32),
+                  paddings=[[top, height - mask_height - top],
+                            [left, width - mask_width - left]])
+
+    return mask
