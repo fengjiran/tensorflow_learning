eef2567f86f7129641112ebd317c606e57243dc1
fengjiran <fengjiran@foxmail.com>
2019-02-20 10:48:38 +0800

    add refine generator

--- Inpainting/two_stage/celebahq/networks.py
+++ Inpainting/two_stage/celebahq/networks.py
@@ -169,3 +169,46 @@ class InpaintingModel():
         images_summary(visual_img, 'groundtruth_masked_inpainted', 4)
 
         return outputs, outputs_merged, gen_loss, dis_loss, coarse_gen_train, coarse_dis_train
+
+    def refine_generator(self, x):
+        with tf.variable_scope('refine_generator'):
+            # encoder
+            x = conv(x, channels=64, kernel=7, stride=1, pad=3,
+                     pad_type='reflect', init_type=self.init_type, name='conv1')
+            x = instance_norm(x, name='in1')
+            x = tf.nn.relu(x)
+
+            x = conv(x, channels=128, kernel=4, stride=2, pad=1,
+                     pad_type='zero', init_type=self.init_type, name='conv2')
+            x = instance_norm(x, name='in2')
+            x = tf.nn.relu(x)
+
+            x = conv(x, channels=256, kernel=4, stride=2, pad=1,
+                     pad_type='zero', init_type=self.init_type, name='conv3')
+            x = instance_norm(x, name='in3')
+            x = tf.nn.relu(x)
+
+            # resnet block
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block1')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block2')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block3')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block4')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block5')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block6')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block7')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block8')
+
+            # decoder
+            x = deconv(x, channels=128, kernel=4, stride=2, init_type=self.init_type, name='deconv1')
+            x = instance_norm(x, name='in4')
+            x = tf.nn.relu(x)
+
+            x = deconv(x, channels=64, kernel=4, stride=2, init_type=self.init_type, name='deconv2')
+            x = instance_norm(x, name='in5')
+            x = tf.nn.relu(x)
+
+            x = conv(x, channels=3, kernel=7, stride=1, pad=3,
+                     pad_type='reflect', init_type=self.init_type, name='conv4')
+            x = tf.nn.tanh(x)
+
+            return x  # [-1, 1]
