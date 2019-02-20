ed3cd97a942339a41379c50a230ff2ab4700af3a
fengjiran <fengjiran@foxmail.com>
2019-02-20 09:50:50 +0800

    import packages

--- Inpainting/two_stage/celebahq/networks.py
+++ Inpainting/two_stage/celebahq/networks.py
@@ -1,16 +1,16 @@
 from __future__ import print_function
 import tensorflow as tf
 
-from .ops import conv
-from .ops import deconv
-from .ops import resnet_block
-from .ops import instance_norm
+from ops import conv
+from ops import deconv
+from ops import resnet_block
+from ops import instance_norm
 
-from .loss import adversarial_loss
-from .loss import perceptual_loss
-from .loss import style_loss
+from loss import adversarial_loss
+from loss import perceptual_loss
+from loss import style_loss
 
-from .utils import images_summary
+from utils import images_summary
 
 
 class InpaintingModel():
