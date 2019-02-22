91966dcab7293218a210d9917f23f00b51f5407d
fengjiran <fengjiran@foxmail.com>
2019-02-22 10:27:23 +0800

    build

--- Inpainting/two_stage/celebahq/coarse_refine.py
+++ Inpainting/two_stage/celebahq/coarse_refine.py
@@ -1,8 +1,22 @@
 import os
+import platform as pf
+import yaml
 import numpy as np
 import tensorflow as tf
 from networks import InpaintingModel
 from dataset import Dataset
+
+with open('config.yaml', 'r') as f:
+    cfg = yaml.load(f)
+
+if pf.system() == 'Windows':
+    pass
+elif pf.system() == 'Linux':
+    if pf.node() == 'icie-Precision-Tower-7810':
+        train_flist = cfg['FLIST_LINUX_7810']
+        log_dir = cfg['LOG_DIR_LINUX_7810']
+    elif pf.node() == 'icie-Precision-T7610':
+        pass
 
 
 class CoarseRefine():
@@ -15,7 +29,7 @@ class CoarseRefine():
 
     def train(self):
         images, masks, train_iterator = self.train_dataset.load_item()
-        flist = self.train_dataset.load_flist(self.cfg['FLIST'])
+        flist = self.train_dataset.load_flist(train_flist)
         num_batch = len(flist) // self.cfg['BATCH_SIZE']
 
         coarse_outputs, coarse_outputs_merged, coarse_gen_loss, coarse_dis_loss, coarse_gen_train, coarse_dis_train =\
@@ -33,7 +47,7 @@ class CoarseRefine():
         with tf.Session(config=config) as sess:
             sess.run(train_iterator.initializer, feed_dict={self.train_dataset.train_filenames: flist})
 
-            summary_writer = tf.summary.FileWriter(self.cfg['LOGDIR'], sess.graph)
+            summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
 
             sess.run(tf.global_variables_initializer())
 
@@ -58,4 +72,6 @@ class CoarseRefine():
 
 
 if __name__ == '__main__':
-    pass
+    # model = CoarseRefine(cfg)
+    # model.train()
+    print(cfg['GAN_LOSS'] == 'nsgan')
