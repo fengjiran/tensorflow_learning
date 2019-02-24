01a80cb1458d434760af457aa408f71406baaaa1
fengjiran <fengjiran@foxmail.com>
2019-02-24 21:09:03 +0800

    build model

--- Inpainting/two_stage/celebahq/coarse_refine.py
+++ Inpainting/two_stage/celebahq/coarse_refine.py
@@ -5,6 +5,7 @@ import numpy as np
 import tensorflow as tf
 from networks import InpaintingModel
 from dataset import Dataset
+from utils import Progbar
 
 with open('config.yaml', 'r') as f:
     cfg = yaml.load(f)
@@ -13,7 +14,7 @@ if pf.system() == 'Windows':
     pass
 elif pf.system() == 'Linux':
     if pf.node() == 'icie-Precision-Tower-7810':
-        train_flist = cfg['FLIST_LINUX_7810']
+        # train_flist = cfg['FLIST_LINUX_7810']
         log_dir = cfg['LOG_DIR_LINUX_7810']
     elif pf.node() == 'icie-Precision-T7610':
         pass
@@ -29,46 +30,58 @@ class CoarseRefine():
 
     def train(self):
         images, masks, train_iterator = self.train_dataset.load_item()
-        flist = self.train_dataset.load_flist(train_flist)
-        num_batch = len(flist) // self.cfg['BATCH_SIZE']
+        flist = self.train_dataset.flist
+        total = len(self.train_dataset)
+        num_batch = total // self.cfg['BATCH_SIZE']
 
-        coarse_outputs, coarse_outputs_merged, coarse_gen_loss, coarse_dis_loss, coarse_gen_train, coarse_dis_train, logs =\
-            self.model.build_coarse_model(images, masks)
+        # coarse model
+        if self.cfg['MODEL'] == 1:
+            # train
+            coarse_outputs, coarse_outputs_merged, coarse_gen_loss, coarse_dis_loss, coarse_gen_train, coarse_dis_train, logs =\
+                self.model.build_coarse_model(images, masks)
 
-        coarse_dis_train_ops = []
-        for i in range(5):
-            coarse_dis_train_ops.append(coarse_dis_train)
-        coarse_dis_train = tf.group(*coarse_dis_train_ops)
+            coarse_dis_train_ops = []
+            for i in range(5):
+                coarse_dis_train_ops.append(coarse_dis_train)
+            coarse_dis_train = tf.group(*coarse_dis_train_ops)
 
-        all_summary = tf.summary.merge_all()
+            all_summary = tf.summary.merge_all()
 
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True
-        with tf.Session(config=config) as sess:
-            sess.run(train_iterator.initializer, feed_dict={self.train_dataset.train_filenames: flist})
+            config = tf.ConfigProto()
+            config.gpu_options.allow_growth = True
+            with tf.Session(config=config) as sess:
+                sess.run(train_iterator.initializer, feed_dict={self.train_dataset.train_filenames: flist})
 
-            summary_writer = tf.summary.FileWriter(log_dir)
+                summary_writer = tf.summary.FileWriter(log_dir)
 
-            sess.run(tf.global_variables_initializer())
+                sess.run(tf.global_variables_initializer())
 
-            step = 0
-            while True:
-                _, _, gen_loss, dis_loss = sess.run([coarse_dis_train,
-                                                     coarse_gen_train,
-                                                     coarse_gen_loss,
-                                                     coarse_dis_loss])
-                print('Epoch: {}, Iter: {}, coarse_gen_loss: {}, coarse_dis_loss: {}'.format(
-                    step // num_batch + 1,
-                    step,
-                    gen_loss,
-                    dis_loss
-                ))
+                epoch = 0
+                keep_training = True
+                step = 0
+                while keep_training:
+                    epoch += 1
+                    print('\n\nTraining epoch: %d' % epoch)
 
-                if step % 200 == 0:
-                    summary = sess.run(all_summary)
-                    summary_writer.add_summary(summary, step)
+                    progbar = Progbar(total, width=20, stateful_metrics=['epoch', 'iter'])
 
-                step += 1
+                    for i in range(num_batch):
+                        _, _, gen_loss, dis_loss = sess.run([coarse_dis_train,
+                                                             coarse_gen_train,
+                                                             coarse_gen_loss,
+                                                             coarse_dis_loss])
+                        print('Epoch: {}, Iter: {}, coarse_gen_loss: {}, coarse_dis_loss: {}'.format(
+                            step // num_batch + 1,
+                            step,
+                            gen_loss,
+                            dis_loss
+                        ))
+
+                        if step % 200 == 0:
+                            summary = sess.run(all_summary)
+                            summary_writer.add_summary(summary, step)
+
+                        step += 1
 
 
 if __name__ == '__main__':
