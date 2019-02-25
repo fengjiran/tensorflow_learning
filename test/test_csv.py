43c7bc75c0cce5d4593edda5155f3677fda27450
fengjiran <fengjiran@foxmail.com>
2019-02-25 22:38:43 +0800

    test csv

--- test/test_csv.py
+++ test/test_csv.py
@@ -7,3 +7,12 @@ with open('log.csv', 'a+') as f:
                       'gen_l1_loss',
                       'gen_style_loss',
                       'gen_content_loss'])
+
+for i in range(10):
+    with open('log.csv', 'a+') as f:
+        mywrite = csv.writer(f)
+        mywrite.writerow([1, 2, 3, 4, 5])
+
+
+# with open('log', 'a+') as f:
+#     print >> f, 'The first time to pretrain!'
