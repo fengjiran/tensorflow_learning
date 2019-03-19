5b80ef7ea280e17541192ac2feb8869552ae3795
fengjiran <fengjiran@foxmail.com>
2019-03-19 10:58:56 +0800

    test

--- test/tf_canny.py
+++ test/tf_canny.py
@@ -5,72 +5,54 @@ from skimage.feature import canny
 import tensorflow as tf
 
 
-def canny_wrap(image, sigma=1.0, mask=None, use_quantiles=False, low_threshold=None, high_threshold=None):
+def canny_wrap(image, sigma, mask=None, use_quantiles=False, low_threshold=None, high_threshold=None):
     return canny(image, sigma, low_threshold, high_threshold, mask, use_quantiles)
 
 
-def tf_canny(image, sigma, mask):
-    edge = tf.py_func(func=canny,
-                      inp=[image, sigma, mask],
+def tf_canny(image, sigma):
+    edge = tf.py_func(func=canny_wrap,
+                      inp=[image, sigma],
                       Tout=tf.bool)
     return edge
 
 
-def my_func(a, b=np.array(2, dtype=np.float32), c=np.array(3, dtype=np.float32)):
-    return a + b + c
-
-
-def tf_func(a, c):
-    d = tf.py_func(func=my_func,
-                   inp=[a, c],
-                   Tout=tf.float32)
-    return d
-
-
 if __name__ == '__main__':
-    a = tf.placeholder(tf.float32, [])
-    c = tf.placeholder(tf.float32, [])
-    re = tf_func(a, c)
+    image = tf.placeholder(tf.float32, [157, 157])
+    # mask = tf.ones([157, 157], dtype=tf.bool)
+    edge1 = tf_canny(image, 1.0)
+    edge2 = tf_canny(image, 3.0)
+
+    # Generate noisy image of a square
+    im = np.zeros((128, 128))
+    im[32:-32, 32:-32] = 1
+
+    im = ndimage.rotate(im, 15, mode='constant')
+    im = ndimage.gaussian_filter(im, 4)
+    im += 0.2 * np.random.random(im.shape)
 
     with tf.Session() as sess:
-        print(sess.run(re, feed_dict={a: np.array(1, dtype=np.float32), c: np.array(4, dtype=np.float32)}))
+        a, b = sess.run([edge1, edge2], feed_dict={image: im})
+        print(a.shape)
 
-    # image = tf.placeholder(tf.float32, [157, 157])
-    # mask = tf.zeros([157, 157], dtype=tf.bool)
-    # edge1 = tf_canny(image, 1.0, mask)
-    # edge2 = tf_canny(image, 3.0, mask)
+        # display results
+        plt.figure(figsize=(8, 3))
 
-    # # Generate noisy image of a square
-    # im = np.zeros((128, 128))
-    # im[32:-32, 32:-32] = 1
+        plt.subplot(131)
+        plt.imshow(im, cmap=plt.cm.jet)
+        plt.axis('off')
+        plt.title('noisy image', fontsize=20)
 
-    # im = ndimage.rotate(im, 15, mode='constant')
-    # im = ndimage.gaussian_filter(im, 4)
-    # im += 0.2 * np.random.random(im.shape)
+        plt.subplot(132)
+        plt.imshow(a, cmap=plt.cm.gray)
+        plt.axis('off')
+        plt.title('Canny filter, $\sigma=1$', fontsize=20)
 
-    # with tf.Session() as sess:
-    #     a, b = sess.run([edge1, edge2], feed_dict={image: im})
-    #     print(a.shape)
+        plt.subplot(133)
+        plt.imshow(b, cmap=plt.cm.gray)
+        plt.axis('off')
+        plt.title('Canny filter, $\sigma=3$', fontsize=20)
 
-    #     # display results
-    #     plt.figure(figsize=(8, 3))
+        plt.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9,
+                            bottom=0.02, left=0.02, right=0.98)
 
-    #     plt.subplot(131)
-    #     plt.imshow(im, cmap=plt.cm.jet)
-    #     plt.axis('off')
-    #     plt.title('noisy image', fontsize=20)
-
-    #     plt.subplot(132)
-    #     plt.imshow(a, cmap=plt.cm.gray)
-    #     plt.axis('off')
-    #     plt.title('Canny filter, $\sigma=1$', fontsize=20)
-
-    #     plt.subplot(133)
-    #     plt.imshow(b, cmap=plt.cm.gray)
-    #     plt.axis('off')
-    #     plt.title('Canny filter, $\sigma=3$', fontsize=20)
-
-    #     plt.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9,
-    #                         bottom=0.02, left=0.02, right=0.98)
-
-    #     plt.show()
+        plt.show()
