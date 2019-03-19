67f13bf1baee08440c92e3bcb4f48da8cfba224f
fengjiran <fengjiran@foxmail.com>
2019-03-19 15:22:57 +0800

    image summaries

--- Inpainting/edge/utils.py
+++ Inpainting/edge/utils.py
@@ -16,3 +16,18 @@ def create_mask(height, width, mask_height, mask_width, x=None, y=None):
     mask = tf.expand_dims(mask, 0)
     mask = tf.expand_dims(mask, -1)
     return mask  # [1, height, width, 1]
+
+
+def images_summary(images, name, max_outs):
+    """Summary images.
+
+    **Note** that images should be scaled to [-1, 1] for 'RGB' or 'BGR',
+    [0, 1] for 'GREY'.
+    :param images: images tensor (in NHWC format)
+    :param name: name of images summary
+    :param max_outs: max_outputs for images summary
+    :param color_format: 'BGR', 'RGB' or 'GREY'
+    :return: None
+    """
+    img = (images + 1) / 2.
+    tf.summary.image(name, img, max_outs)
