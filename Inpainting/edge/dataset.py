9af4d3382c8eb8040f9e9ac2a22a0baac62eeb4e
fengjiran <fengjiran@foxmail.com>
2019-03-20 15:03:15 +0800

    mask

--- Inpainting/edge/dataset.py
+++ Inpainting/edge/dataset.py
@@ -73,7 +73,7 @@ class Dataset():
 
         # in test mode images are masked (with masked regions),
         # using 'mask' parameter prevents canny to detect edges for the masked regions
-        mask = tf.cast(tf.ones([shape[1], shape[2]]), dtype=tf.bool) if self.training else tf.cast(mask, dtype=tf.bool)
+        mask = tf.cast(tf.ones([shape[1], shape[2]]), dtype=tf.bool) if mask == None else tf.cast(mask, dtype=tf.bool)
 
         img_edges = tf.map_fn(fn=lambda im: tf_canny(im, sigma, mask),
                               elems=img_gray,
