e0e746711df72d531b1aa9e53b159cafbe67d04a
fengjiran <fengjiran@foxmail.com>
2019-03-21 18:29:38 +0800

    test mask

--- test/test_resize_mask.py
+++ test/test_resize_mask.py
@@ -9,22 +9,33 @@ img = imread(path)
 img1 = imresize(img, [256, 256])
 img1 = (img1 > 3).astype(np.uint8) * 255
 
-plt.figure(figsize=(8, 3))
-
-plt.subplot(121)
-plt.imshow(img, cmap=plt.cm.gray)
-plt.axis('off')
-plt.title('origin', fontsize=20)
-
-plt.subplot(122)
-plt.imshow(img1, cmap=plt.cm.gray)
-plt.axis('off')
-plt.title('resize', fontsize=20)
-
-plt.show()
-
 
 a = tf.read_file(path)
 b = tf.image.decode_png(a)
 b = tf.reshape(b, [1, 512, 512, 1])
 b = tf.image.resize_area(b, [256, 256])
+b = tf.cast(tf.greater(b, 3), dtype=tf.uint8) * 255  # {0, 255}
+print(b.get_shape().as_list())
+
+with tf.Session() as sess:
+    re = sess.run(b)
+    print(re)
+
+    plt.figure(figsize=(8, 3))
+
+    plt.subplot(131)
+    plt.imshow(img, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.title('origin', fontsize=20)
+
+    plt.subplot(132)
+    plt.imshow(img1, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.title('resize', fontsize=20)
+
+    plt.subplot(133)
+    plt.imshow(re, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.title('resize2', fontsize=20)
+
+    plt.show()
