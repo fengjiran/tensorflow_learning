bfa7b435650bf78435b893cb53e0f5bc86b17ef7
fengjiran <fengjiran@foxmail.com>
2019-03-21 18:49:57 +0800

    test

--- test/test_resize_mask.py
+++ test/test_resize_mask.py
@@ -12,6 +12,7 @@ img1 = (img1 > 3).astype(np.uint8) * 255
 
 a = tf.read_file(path)
 b = tf.image.decode_png(a)
+print(b.get_shape().as_list())
 b = tf.reshape(b, [1, 512, 512, 1])
 b = tf.image.resize_area(b, [256, 256])
 b = tf.cast(tf.greater(b, 3), dtype=tf.uint8) * 255  # {0, 255}
@@ -20,6 +21,7 @@ print(b.get_shape().as_list())
 with tf.Session() as sess:
     re = sess.run(b)
     print(re)
+    re = np.reshape(re, [256, 256])
 
     plt.figure(figsize=(8, 3))
 
