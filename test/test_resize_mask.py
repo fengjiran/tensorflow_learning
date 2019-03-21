9ed404997a77b34ad16430d5967644c5681c1716
fengjiran <fengjiran@foxmail.com>
2019-03-21 17:26:29 +0800

    test

--- test/test_resize_mask.py
+++ test/test_resize_mask.py
@@ -2,8 +2,10 @@ import numpy as np
 from scipy.misc import imread
 from scipy.misc import imresize
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
-img = imread('F:\\Datasets\\qd_imd\\train\\00002_train.png')
+path = 'F:\\Datasets\\qd_imd\\train\\00002_train.png'
+img = imread(path)
 img1 = imresize(img, [256, 256])
 img1 = (img1 > 3).astype(np.uint8) * 255
 
@@ -20,3 +22,9 @@ plt.axis('off')
 plt.title('resize', fontsize=20)
 
 plt.show()
+
+
+a = tf.read_file(path)
+b = tf.image.decode_png(a)
+b = tf.reshape(b, [1, 512, 512, 1])
+b = tf.image.resize_area(b, [256, 256])
