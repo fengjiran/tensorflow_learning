b0b0eb99acd97f8f77d0b088a393b725a1f128ac
fengjiran <fengjiran@foxmail.com>
2019-03-22 16:29:21 +0800

    networks

--- Inpainting/edge/networks.py
+++ Inpainting/edge/networks.py
@@ -3,3 +3,25 @@ import os
 import tensorflow as tf
 
 from ops import conv
+from ops import deconv
+from ops import resnet_block
+from ops import instance_norm
+
+from loss import adversarial_loss
+
+from utils import images_summary
+
+
+class EdgeModel():
+    """Construct edge model."""
+
+    def __init__(self, config=None):
+        print('Construct the edge model.')
+        self.cfg = config
+        self.init_type = self.cfg['INIT_TYPE']
+
+    def edge_generator(self, x, reuse=None):
+        with tf.variable_scope('edge_generator', reuse=reuse):
+            # encoder
+            x = conv(x, channels=64, kernel=7, stride=1, pad=3,
+                     pad_type='reflect', init_type=self.init_type, name='conv1')
