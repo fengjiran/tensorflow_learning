caf46e307223da4c5c07cf98e0ed8883dcfaa433
fengjiran <fengjiran@foxmail.com>
2019-03-22 16:47:57 +0800

    networks

--- Inpainting/edge/networks.py
+++ Inpainting/edge/networks.py
@@ -25,3 +25,25 @@ class EdgeModel():
             # encoder
             x = conv(x, channels=64, kernel=7, stride=1, pad=3,
                      pad_type='reflect', init_type=self.init_type, name='conv1')
+            x = instance_norm(x, name='in1')
+            x = tf.nn.relu(x)
+
+            x = conv(x, channels=128, kernel=4, stride=2, pad=1,
+                     pad_type='reflect', init_type=self.init_type, name='conv2')
+            x = instance_norm(x, name='in2')
+            x = tf.nn.relu(x)
+
+            x = conv(x, channels=256, kernel=4, stride=2, pad=1,
+                     pad_type='zero', init_type=self.init_type, name='conv3')
+            x = instance_norm(x, name='in3')
+            x = tf.nn.relu(x)
+
+            # resnet block
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block1')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block2')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block3')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block4')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block5')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block6')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block7')
+            x = resnet_block(x, out_channels=256, dilation=2, init_type=self.init_type, name='resnet_block8')
