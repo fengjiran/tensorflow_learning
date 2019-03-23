eb202772a67261db0a0b1c0ea913129775e1157a
fengjiran <fengjiran@foxmail.com>
2019-03-23 22:49:06 +0800

    train

--- Inpainting/edge/edge_model.py
+++ Inpainting/edge/edge_model.py
@@ -12,8 +12,19 @@ if pf.system() == 'Windows':
     pass
 elif pf.system() == 'Linux':
     if pf.node() == 'icie-Precision-Tower-7810':
-        # train_flist = cfg['FLIST_LINUX_7810']
         log_dir = cfg['LOG_DIR_LINUX_7810']
         model_dir = cfg['MODEL_PATH_LINUX_7810']
     elif pf.node() == 'icie-Precision-T7610':
+        pass
+
+
+class Edge():
+    """Construct edge model."""
+
+    def __init__(self, config):
+        self.cfg = config
+        self.model = EdgeModel(config)
+        self.dataset = Dataset(config)
+
+    def train(self):
         pass
