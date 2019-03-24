3cb5208692424e4198d8bcd7188fadbe8d5bcd4b
fengjiran <fengjiran@foxmail.com>
2019-03-24 12:23:01 +0800

    build model

--- Inpainting/edge/edge_model.py
+++ Inpainting/edge/edge_model.py
@@ -52,6 +52,41 @@ class EdgeAware():
             feed_dict = {self.dataset.train_filenames: flist,
                          self.dataset.mask_filenames: mask_flist} if cfg['MASK'] == 2 else {self.dataset.train_filenames: flist}
             sess.run(iterators, feed_dict=feed_dict)
+            sess.run(tf.global_variables_initializer())
+            summary_writer = tf.summary.FileWriter(log_dir)
+
+            with open('logs.csv', 'a+') as f:
+                mywrite = csv.writer(f)
+                mywrite.writerow(['dis_loss', 'gen_loss', 'gen_gan_loss', 'gen_fm_loss'])
+            all_summary = tf.summary.merge_all()
+
+            while keep_training:
+                epoch += 1
+                print('\n\nTraining epoch: %d' % epoch)
+                for i in range(num_batch):
+                    _, _, logs_ = sess.run([dis_train, gen_train, logs])
+                    print('Epoch: {}, Iter: {}'.format(epoch, step))
+                    print('-----------dis_loss: {}'.format(logs_[0]))
+                    print('-----------gen_loss: {}'.format(logs_[1]))
+                    print('-----------gen_gan_loss: {}'.format(logs_[2]))
+                    print('-----------gen_fm_loss: {}'.format(logs_[3]))
+
+                    with open('coarse_logs.csv', 'a+') as f:
+                        mywrite = csv.writer(f)
+                        mywrite.writerow(logs_)
+
+                    if step % self.cfg['SUMMARY_INTERVAL'] == 0:
+                        summary = sess.run(all_summary)
+                        summary_writer.add_summary(summary, step)
+
+                    if self.cfg['SAVE_INTERVAL'] and step % self.cfg['SAVE_INTERVAL'] == 0:
+                        self.model.save(sess, saver, model_dir, 'model')
+
+                    if step >= max_iteration:
+                        keep_training = False
+                        break
+
+                    step += 1
 
 
 if __name__ == '__main__':
