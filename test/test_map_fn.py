4f0196926fca954fda6df3effce3409b4d3bbc5f
fengjiran <fengjiran@foxmail.com>
2019-03-25 18:03:34 +0800

    change

--- test/test_map_fn.py
+++ test/test_map_fn.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import numpy as np
 
-a = tf.placeholder(tf.float32, [3])
-b = tf.placeholder(tf.float32, [3])
+a = tf.placeholder(tf.float32, [3, 2])
+b = tf.placeholder(tf.float32, [3, 2])
 
 
 def compute(x, y):
-    return x + y, x - y
+    return tf.reduce_mean(x), tf.reduce_mean(y)  # x + y, x - y
 
 
 acc = tf.map_fn(fn=lambda x: compute(x[0], x[1]),
@@ -14,5 +14,6 @@ acc = tf.map_fn(fn=lambda x: compute(x[0], x[1]),
                 dtype=(tf.float32, tf.float32))
 
 with tf.Session() as sess:
-    print(sess.run(acc, feed_dict={a: np.array([1., 2., 3.], dtype=np.float),
-                                   b: np.array([-1., 1., -1.], dtype=np.float)}))
+    aa = np.array([[1, 2], [3, 4], [5, 6]], dtype=np.float)
+    bb = np.array([[7, 8], [9, 10], [11, 12]], dtype=np.float)
+    print(sess.run(acc, feed_dict={a: aa, b: bb}))
