2a5f45b765048864d09d1992230758662d65c530
fengjiran <fengjiran@foxmail.com>
2019-03-25 14:51:59 +0800

    metrics

--- Inpainting/edge/metrics.py
+++ Inpainting/edge/metrics.py
@@ -3,6 +3,27 @@ import tensorflow as tf
 # use tf.map_fn
 
 
+def edge_accuracy_per_image(real_edge, fake_edge, threshold):
+    real_edge = real_edge > threshold
+    fake_edge = fake_edge > threshold
+
+    relevant = tf.reduce_sum(tf.cast(real_edge, dtype=tf.float32))
+    selected = tf.reduce_sum(tf.cast(fake_edge, dtype=tf.float32))
+
+    precision = 0
+    recall = 0
+
+    if relevant == 0 and selected == 0:
+        precision = 1
+        recall = 1
+    else:
+        true_positive = tf.cast((real_edge == fake_edge) * real_edge, dtype=tf.float32)
+        recall = tf.reduce_sum(true_positive) / (relevant + 1e-8)
+        precision = tf.reduce_sum(true_positive) / (selected + 1e-8)
+
+    return (precision, recall)
+
+
 def edge_accuracy(real_edge, fake_edge, threshold):
     lables = real_edge > threshold
     fake_edge = fake_edge > threshold
