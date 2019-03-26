58ac6423dac39d16013deb0bc1b6574c0320dc0f
fengjiran <fengjiran@foxmail.com>
2019-03-26 15:35:12 +0800

    test

--- Inpainting/edge/test.py
+++ Inpainting/edge/test.py
@@ -11,6 +11,14 @@ from skimage.color import rgb2gray
 import tensorflow as tf
 from networks import EdgeModel
 
+if pf.system() == 'Windows':
+    checkpoint_dir = config['MODEL_PATH_WIN']
+elif pf.system() == 'Linux':
+    if pf.node() == 'icie-Precision-Tower-7810':
+        checkpoint_dir = config['MODEL_PATH_LINUX_7810']
+    elif pf.node() == 'icie-Precision-T7610':
+        pass
+
 with open('config.yaml', 'r') as f:
     cfg = yaml.load(f)
 
@@ -27,3 +35,10 @@ edge_masked = edge * (1 - mask)
 inputs = tf.concat([gray_masked, edge_masked, mask * tf.ones_like(gray)], axis=3)
 
 outputs = edge_model.edge_generator(inputs)
+outputs_merged = outputs * mask + edge * (1 - mask)
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config) as sess:
+    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'edge_generator')
+    assign_ops = []
