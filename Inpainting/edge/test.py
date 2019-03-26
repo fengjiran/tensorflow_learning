fa27f13a73bbc73217823d7066fee3df22d4d1d8
fengjiran <fengjiran@foxmail.com>
2019-03-26 15:39:16 +0800

    test

--- Inpainting/edge/test.py
+++ Inpainting/edge/test.py
@@ -11,16 +11,17 @@ from skimage.color import rgb2gray
 import tensorflow as tf
 from networks import EdgeModel
 
+with open('config.yaml', 'r') as f:
+    cfg = yaml.load(f)
+
 if pf.system() == 'Windows':
-    checkpoint_dir = config['MODEL_PATH_WIN']
+    checkpoint_dir = cfg['MODEL_PATH_WIN']
 elif pf.system() == 'Linux':
     if pf.node() == 'icie-Precision-Tower-7810':
-        checkpoint_dir = config['MODEL_PATH_LINUX_7810']
+        checkpoint_dir = cfg['MODEL_PATH_LINUX_7810']
     elif pf.node() == 'icie-Precision-T7610':
         pass
 
-with open('config.yaml', 'r') as f:
-    cfg = yaml.load(f)
 
 edge_model = EdgeModel(cfg)
 
@@ -42,3 +43,10 @@ config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
     vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'edge_generator')
     assign_ops = []
+    for var in vars_list:
+        vname = var.name
+        from_name = vname
+        var_value = tf.train.load_variable(checkpoint_dir, from_name)
+        assign_ops.append(tf.assign(var, var_value))
+    sess.run(assign_ops)
+    print('Model loaded.')
