49f57b95ad37de130062427c29b84689790bef16
fengjiran <fengjiran@foxmail.com>
2019-04-09 01:08:23 +0800

    test image color domain

--- test/test_blur.py
+++ test/test_blur.py
@@ -24,3 +24,11 @@ def img_kmeans(img_blur, K=8):
 if __name__ == '__main__':
     img = imread('img.png')
     print(img.shape)
+
+    k = 3
+
+    img_color_domain = cv2.medianBlur(img, 25)
+    img_color_domain = img_kmeans(img_color_domain, k)
+    img_color_domain = cv2.medianBlur(img_color_domain, 3)
+
+    print(img_color_domain.shape)
