9d5f47e1a2523c10bf9f4f8062322f70000d5d30
fengjiran <fengjiran@foxmail.com>
2019-04-09 01:44:53 +0800

    test

--- test/test_blur.py
+++ test/test_blur.py
@@ -8,7 +8,7 @@ def img_kmeans(img_blur, K=8):
     Z = img_blur.reshape((-1, 3))
 
     # convert to np.float32
-    Z = np.float32(Z)
+    # Z = np.float32(Z)
 
     # define criteria, number of clusters(K) and apply kmeans()
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
@@ -16,7 +16,7 @@ def img_kmeans(img_blur, K=8):
     ret, label, center = cv2.kmeans(Z, K, None, criteria, 8, cv2.KMEANS_PP_CENTERS)
 
     # Now convert back into uint8, and make original image
-    center = np.uint8(center)
+    # center = np.uint8(center)
     res = center[label.flatten()]
     res = res.reshape((img_blur.shape))
     return res
@@ -24,6 +24,7 @@ def img_kmeans(img_blur, K=8):
 
 if __name__ == '__main__':
     img = imread('test.jpg')
+    img = img / 255.
     print(img.shape)
 
     k = 3
