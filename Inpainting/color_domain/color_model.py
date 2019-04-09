cecd9bd9f469fb3dd19dab7a61059c965abbf6bd
fengjiran <fengjiran@foxmail.com>
2019-04-09 12:21:23 +0800

    build model

--- Inpainting/color_domain/color_model.py
+++ Inpainting/color_domain/color_model.py
@@ -16,3 +16,15 @@ elif pf.system() == 'Linux':
         model_dir = cfg['MODEL_PATH_LINUX_7810']
     elif pf.node() == 'icie-Precision-T7610':
         pass
+
+
+class ColorAware():
+    """Construct color domain model."""
+
+    def __init__(self, config):
+        self.cfg = config
+        self.model = ColorModel(config)
+        self.dataset = Dataset(config)
+
+    def train(self):
+        pass
