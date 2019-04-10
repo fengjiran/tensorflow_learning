6bb58d6f2a95ee58ade76865f9455520f5dba7a9
fengjiran <fengjiran@foxmail.com>
2019-04-10 16:52:04 +0800

    fix bugs

--- Inpainting/edge/edge_model.py
+++ Inpainting/edge/edge_model.py
@@ -62,7 +62,7 @@ class EdgeAware():
                 epoch = 0
                 sess.run(tf.global_variables_initializer())
             else:
-                saver.restore(os.path.join(model_dir, 'model'))
+                saver.restore(sess, os.path.join(model_dir, 'model'))
                 step = tf.train.load_variable(os.path.join(model_dir, 'model'), 'gen_global_step')
                 epoch = step // num_batch
 
