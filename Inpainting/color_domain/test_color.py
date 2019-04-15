38ba460bd5f0a15465d332318543bfc87054d49c
fengjiran <fengjiran@foxmail.com>
2019-04-16 02:39:43 +0800

    test

--- Inpainting/color_domain/test_color.py
+++ Inpainting/color_domain/test_color.py
@@ -8,3 +8,11 @@ from .networks import ColorModel
 
 with open('config.yaml', 'r') as f:
     cfg = yaml.load(f)
+
+if pf.system() == 'Windows':
+    checkpoint_dir = cfg['MODEL_PATH_WIN']
+elif pf.system() == 'Linux':
+    if pf.node() == 'icie-Precision-Tower-7810':
+        checkpoint_dir = cfg['MODEL_PATH_LINUX_7810']
+    elif pf.node() == 'icie-Precision-T7610':
+        checkpoint_dir = cfg['MODEL_PATH_LINUX_7610']
