ba7b1ae35aeac7d7ced9c90ee1f1953cbe1221d3
fengjiran <fengjiran@foxmail.com>
2019-04-16 09:14:14 +0800

    test color model

--- Inpainting/color_domain/test_color.py
+++ Inpainting/color_domain/test_color.py
@@ -19,3 +19,24 @@ elif pf.system() == 'Linux':
 
 
 model = ColorModel(cfg)
+
+image = tf.placeholder(tf.float32, [1, cfg['INPUT_SIZE'], cfg['INPUT_SIZE'], 3])
+color_domain = tf.placeholder(tf.float32, [1, cfg['INPUT_SIZE'], cfg['INPUT_SIZE'], 3])
+mask = tf.placeholder(tf.float32, [1, cfg['INPUT_SIZE'], cfg['INPUT_SIZE'], 1])
+
+output = model.test_model(image, color_domain, mask)
+
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session() as sess:
+    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'color_generator')
+    assign_ops = []
+    for var in vars_list:
+        vname = var.name
+        print(vname)
+        from_name = vname
+        var_value = tf.train.load_variable(os.path.join(checkpoint_dir, 'model'), from_name)
+        assign_ops.append(tf.assign(var, var_value))
+    sess.run(assign_ops)
+    print('Model loaded.')
