6c31f96d47bfd0791fdfbbfadb499b466186fbdc
fengjiran <fengjiran@foxmail.com>
2019-04-16 11:20:28 +0800

    test

--- Inpainting/color_domain/test_color.py
+++ Inpainting/color_domain/test_color.py
@@ -38,9 +38,15 @@ def load_image(cfg, image_path):
     blur_factor1 = cfg['BLUR_FACTOR1']
     blur_factor2 = cfg['BLUR_FACTOR2']
     k = cfg['K']
-    image = imread(image_path)
+    image = imread(image_path)  # [0, 255]
     image = cv2.resize(image, (cfg['INPUT_SIZE'], cfg['INPUT_SIZE']), interpolation=cv2.INTER_AREA)  # (256, 256, 3)
-    img_color_domain = get_color_domain(image, blur_factor1, blur_factor2, k)
+    img_color_domain = get_color_domain(image, blur_factor1, blur_factor2, k)  # [0, 1]
+
+    image = image / 127.5 - 1
+
+    image = np.expand_dims(image, 0)
+    img_color_domain = np.expand_dims(img_color_domain, 0)
+
     return image, img_color_domain  # (1, 256, 256, 3)
 
 
@@ -56,50 +62,61 @@ if __name__ == '__main__':
         elif pf.node() == 'icie-Precision-T7610':
             checkpoint_dir = cfg['MODEL_PATH_LINUX_7610']
 
+    ############################# load the data #########################################
     mask_type = 1
+    mask_path = 'F:\\Datasets\\qd_imd\\train\\00001_train.png'
+    image_path = 'F:\\Datasets\\celebahq\\img00000000.png'
+    img_mask = load_mask(cfg, mask_type, mask_path)
+    img, img_color_domain = load_image(cfg, image_path)
 
+    ########################### construct the model #####################################
     model = ColorModel(cfg)
-
     image = tf.placeholder(tf.float32, [1, cfg['INPUT_SIZE'], cfg['INPUT_SIZE'], 3])
     color_domain = tf.placeholder(tf.float32, [1, cfg['INPUT_SIZE'], cfg['INPUT_SIZE'], 3])
     mask = tf.placeholder(tf.float32, [1, cfg['INPUT_SIZE'], cfg['INPUT_SIZE'], 1])
-
     output = model.test_model(image, color_domain, mask)
 
-    # config = tf.ConfigProto()
-    # config.gpu_options.allow_growth = True
-    # with tf.Session() as sess:
-    #     vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'color_generator')
-    #     assign_ops = []
-    #     for var in vars_list:
-    #         vname = var.name
-    #         print(vname)
-    #         from_name = vname
-    #         var_value = tf.train.load_variable(os.path.join(checkpoint_dir, 'model'), from_name)
-    #         assign_ops.append(tf.assign(var, var_value))
-    #     sess.run(assign_ops)
-    #     print('Model loaded.')
+    feed_dict = {image: img, color_domain: img_color_domain, mask: img_mask}
+    #####################################################################################
 
-    mask_path = 'F:\\Datasets\\qd_imd\\train\\00001_train.png'
-    image_path = 'F:\\Datasets\\celebahq\\img00000000.png'
-    mask = load_mask(cfg, mask_type, mask_path)
-    image, img_color_domain = load_image(cfg, image_path)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session() as sess:
+        vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'color_generator')
+        assign_ops = []
+        for var in vars_list:
+            vname = var.name
+            # print(vname)
+            from_name = vname
+            var_value = tf.train.load_variable(os.path.join(checkpoint_dir, 'model'), from_name)
+            assign_ops.append(tf.assign(var, var_value))
+        sess.run(assign_ops)
+        print('Model loaded.')
 
-    plt.figure(figsize=(8, 3))
+        inpainted_color = sess.run(output, feed_dict=feed_dict)
 
-    plt.subplot(131)
-    plt.imshow(image[0])
-    plt.axis('off')
-    plt.title('image', fontsize=20)
+        img = (img + 1) / 2.
 
-    plt.subplot(132)
-    plt.imshow(img_color_domain[0])
-    plt.axis('off')
-    plt.title('color domain', fontsize=20)
+        plt.figure(figsize=(8, 3))
 
-    plt.subplot(133)
-    plt.imshow(mask[0, :, :, 0], cmap=plt.cm.gray)
-    plt.axis('off')
-    plt.title('mask', fontsize=20)
+        plt.subplot(141)
+        plt.imshow(img[0])
+        plt.axis('off')
+        plt.title('image', fontsize=20)
 
-    plt.show()
+        plt.subplot(142)
+        plt.imshow(img_color_domain[0])
+        plt.axis('off')
+        plt.title('color domain', fontsize=20)
+
+        plt.subplot(143)
+        plt.imshow(img_mask[0, :, :, 0], cmap=plt.cm.gray)
+        plt.axis('off')
+        plt.title('mask', fontsize=20)
+
+        plt.subplot(144)
+        plt.imshow(inpainted_color[0])
+        plt.axis('off')
+        plt.title('inpainted', fontsize=20)
+
+        plt.show()
