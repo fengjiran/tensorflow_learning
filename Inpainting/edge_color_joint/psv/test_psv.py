98c7bcf4742a51cf029a05649094ea115120da54
fengjiran <fengjiran@foxmail.com>
2019-04-17 16:50:11 +0800

    test psv

--- Inpainting/edge_color_joint/psv/test_psv.py
+++ Inpainting/edge_color_joint/psv/test_psv.py
@@ -1,15 +1,59 @@
+import os
+import argparse
 import numpy as np
+import cv2
 from scipy.misc import imread
 from scipy.misc import imsave
 
-img = imread('img.JPG')
-print(img.shape)
-a = img[:, 0:537, :]
-b = img[:, 199:736, :]
-c = img[:, 399:936, :]
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, help='path to the dataset')
+parser.add_argument('--output', type=str, help='path to the output dataset')
+args = parser.parse_args()
 
-print(a.shape, b.shape, c.shape)
+ext = {'.jpg', '.png', '.JPG'}
 
-imsave('a.JPG', a)
-imsave('b.JPG', b)
-imsave('c.JPG', c)
+i = 0
+for root, dirs, files in os.walk(args.path):
+    print('loading ' + root)
+    for file in files:
+        if os.path.splitext(file)[1] in ext:
+            file_path = os.path.join(root, file)
+
+            print('handle the {}st image.'.format(i + 1))
+            img = imread(file_path)
+            a = img[:, 0:537, :]
+            b = img[:, 199:736, :]
+            c = img[:, 399:936, :]
+
+            a = cv2.resize(a, (256, 256), interpolation=cv2.INTER_AREA)
+            b = cv2.resize(b, (256, 256), interpolation=cv2.INTER_AREA)
+            c = cv2.resize(c, (256, 256), interpolation=cv2.INTER_AREA)
+
+            imsave(os.path.join(args.output, 'psv%05d.JPG' % (i * 3)), a)
+            imsave(os.path.join(args.output, 'psv%05d.JPG' % (i * 3 + 1)), b)
+            imsave(os.path.join(args.output, 'psv%05d.JPG' % (i * 3 + 2)), c)
+
+            i = i + 1
+
+
+# img = imread('img.JPG')
+# print(img.shape)
+# a = img[:, 0:537, :]
+# b = img[:, 199:736, :]
+# c = img[:, 399:936, :]
+
+# print(a.shape, b.shape, c.shape)
+
+# a = cv2.resize(a, (256, 256), interpolation=cv2.INTER_AREA)
+# b = cv2.resize(b, (256, 256), interpolation=cv2.INTER_AREA)
+# c = cv2.resize(c, (256, 256), interpolation=cv2.INTER_AREA)
+
+# for i in range(20):
+#     filename = 'psv%05d.JPG' % i
+#     print(filename)
+
+# path = 'F:\\Datasets\\psv\\train'
+
+# imsave(os.path.join(path, 'a.JPG'), a)
+# imsave(os.path.join(path, 'b.JPG'), b)
+# imsave(os.path.join(path, 'c.JPG'), c)
