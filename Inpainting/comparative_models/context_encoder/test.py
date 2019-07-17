5a24e0de0f3afce2e113008a78ce0358d64801cb
fengjiran <fengjiran@foxmail.com>
2019-07-17 22:12:28 +0800

    test

--- Inpainting/comparative_models/context_encoder/test.py
+++ Inpainting/comparative_models/context_encoder/test.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+import numpy as np
+from psnr import psnr
 import argparse
 import os
 import random
@@ -16,10 +18,9 @@ from torch.autograd import Variable
 from model import _netG
 
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset',  default='streetview', help='cifar10 | lsun | imagenet | folder | lfw ')
-parser.add_argument('--dataroot',  default='dataset/val', help='path to dataset')
+parser.add_argument('--dataset', default='streetview', help='cifar10 | lsun | imagenet | folder | lfw ')
+parser.add_argument('--dataroot', default='dataset/val', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=128, help='the height / width of the input image to network')
@@ -37,23 +38,23 @@ parser.add_argument('--netD', default='', help="path to netD (to continue traini
 parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
 
-parser.add_argument('--nBottleneck', type=int,default=4000,help='of dim for bottleneck of encoder')
-parser.add_argument('--overlapPred',type=int,default=4,help='overlapping edges')
-parser.add_argument('--nef',type=int,default=64,help='of encoder filters in first conv layer')
-parser.add_argument('--wtl2',type=float,default=0.999,help='0 means do not use else use with this weight')
+parser.add_argument('--nBottleneck', type=int, default=4000, help='of dim for bottleneck of encoder')
+parser.add_argument('--overlapPred', type=int, default=4, help='overlapping edges')
+parser.add_argument('--nef', type=int, default=64, help='of encoder filters in first conv layer')
+parser.add_argument('--wtl2', type=float, default=0.999, help='0 means do not use else use with this weight')
 opt = parser.parse_args()
 print(opt)
 
 
 netG = _netG(opt)
-netG.load_state_dict(torch.load(opt.netG,map_location=lambda storage, location: storage)['state_dict'])
+netG.load_state_dict(torch.load(opt.netG, map_location=lambda storage, location: storage)['state_dict'])
 netG.eval()
 
 transform = transforms.Compose([transforms.Scale(opt.imageSize),
-                                    transforms.CenterCrop(opt.imageSize),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-dataset = dset.ImageFolder(root=opt.dataroot, transform=transform )
+                                transforms.CenterCrop(opt.imageSize),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+dataset = dset.ImageFolder(root=opt.dataroot, transform=transform)
 assert dataset
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
                                          shuffle=True, num_workers=int(opt.workers))
@@ -61,13 +62,13 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
 
 input_real = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
 input_cropped = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
-real_center = torch.FloatTensor(opt.batchSize, 3, opt.imageSize/2, opt.imageSize/2)
+real_center = torch.FloatTensor(opt.batchSize, 3, opt.imageSize / 2, opt.imageSize / 2)
 
 criterionMSE = nn.MSELoss()
 
 if opt.cuda:
     netG.cuda()
-    input_real, input_cropped = input_real.cuda(),input_cropped.cuda()
+    input_real, input_cropped = input_real.cuda(), input_cropped.cuda()
     criterionMSE.cuda()
     real_center = real_center.cuda()
 
@@ -80,44 +81,44 @@ real_cpu, _ = dataiter.next()
 
 input_real.data.resize_(real_cpu.size()).copy_(real_cpu)
 input_cropped.data.resize_(real_cpu.size()).copy_(real_cpu)
-real_center_cpu = real_cpu[:,:,opt.imageSize/4:opt.imageSize/4+opt.imageSize/2,opt.imageSize/4:opt.imageSize/4+opt.imageSize/2]
+real_center_cpu = real_cpu[:, :, opt.imageSize / 4:opt.imageSize / 4 +
+                           opt.imageSize / 2, opt.imageSize / 4:opt.imageSize / 4 + opt.imageSize / 2]
 real_center.data.resize_(real_center_cpu.size()).copy_(real_center_cpu)
 
-input_cropped.data[:,0,opt.imageSize/4+opt.overlapPred:opt.imageSize/4+opt.imageSize/2-opt.overlapPred,opt.imageSize/4+opt.overlapPred:opt.imageSize/4+opt.imageSize/2-opt.overlapPred] = 2*117.0/255.0 - 1.0
-input_cropped.data[:,1,opt.imageSize/4+opt.overlapPred:opt.imageSize/4+opt.imageSize/2-opt.overlapPred,opt.imageSize/4+opt.overlapPred:opt.imageSize/4+opt.imageSize/2-opt.overlapPred] = 2*104.0/255.0 - 1.0
-input_cropped.data[:,2,opt.imageSize/4+opt.overlapPred:opt.imageSize/4+opt.imageSize/2-opt.overlapPred,opt.imageSize/4+opt.overlapPred:opt.imageSize/4+opt.imageSize/2-opt.overlapPred] = 2*123.0/255.0 - 1.0
+input_cropped.data[:, 0, opt.imageSize / 4 + opt.overlapPred:opt.imageSize / 4 + opt.imageSize / 2 - opt.overlapPred,
+                   opt.imageSize / 4 + opt.overlapPred:opt.imageSize / 4 + opt.imageSize / 2 - opt.overlapPred] = 2 * 117.0 / 255.0 - 1.0
+input_cropped.data[:, 1, opt.imageSize / 4 + opt.overlapPred:opt.imageSize / 4 + opt.imageSize / 2 - opt.overlapPred,
+                   opt.imageSize / 4 + opt.overlapPred:opt.imageSize / 4 + opt.imageSize / 2 - opt.overlapPred] = 2 * 104.0 / 255.0 - 1.0
+input_cropped.data[:, 2, opt.imageSize / 4 + opt.overlapPred:opt.imageSize / 4 + opt.imageSize / 2 - opt.overlapPred,
+                   opt.imageSize / 4 + opt.overlapPred:opt.imageSize / 4 + opt.imageSize / 2 - opt.overlapPred] = 2 * 123.0 / 255.0 - 1.0
 
 fake = netG(input_cropped)
-errG = criterionMSE(fake,real_center)
+errG = criterionMSE(fake, real_center)
 
 recon_image = input_cropped.clone()
-recon_image.data[:,:,opt.imageSize/4:opt.imageSize/4+opt.imageSize/2,opt.imageSize/4:opt.imageSize/4+opt.imageSize/2] = fake.data
+recon_image.data[:, :, opt.imageSize / 4:opt.imageSize / 4 + opt.imageSize /
+                 2, opt.imageSize / 4:opt.imageSize / 4 + opt.imageSize / 2] = fake.data
 
-vutils.save_image(real_cpu,'val_real_samples.png',normalize=True)
-vutils.save_image(input_cropped.data,'val_cropped_samples.png',normalize=True)
-vutils.save_image(recon_image.data,'val_recon_samples.png',normalize=True)
-p=0
-l1=0
-l2=0
+vutils.save_image(real_cpu, 'val_real_samples.png', normalize=True)
+vutils.save_image(input_cropped.data, 'val_cropped_samples.png', normalize=True)
+vutils.save_image(recon_image.data, 'val_recon_samples.png', normalize=True)
+p = 0
+l1 = 0
+l2 = 0
 fake = fake.data.numpy()
 real_center = real_center.data.numpy()
-from psnr import psnr
-import numpy as np
 
 t = real_center - fake
 l2 = np.mean(np.square(t))
 l1 = np.mean(np.abs(t))
-real_center = (real_center+1)*127.5
-fake = (fake+1)*127.5
+real_center = (real_center + 1) * 127.5
+fake = (fake + 1) * 127.5
 
 for i in range(opt.batchSize):
-    p = p + psnr(real_center[i].transpose(1,2,0) , fake[i].transpose(1,2,0))
+    p = p + psnr(real_center[i].transpose(1, 2, 0), fake[i].transpose(1, 2, 0))
 
 print(l2)
 
 print(l1)
 
-print(p/opt.batchSize)
-
-
-
+print(p / opt.batchSize)
