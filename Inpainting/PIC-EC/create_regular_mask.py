055675eef93be7f860054c61f1f22bce2901bd0a
fengjiran <fengjiran@foxmail.com>
2019-07-24 13:18:19 +0800

    create regular mask

--- Inpainting/PIC-EC/create_regular_mask.py
+++ Inpainting/PIC-EC/create_regular_mask.py
@@ -3,7 +3,7 @@ import numpy as np
 from imageio import imwrite
 
 # smaple_dir = 'E:\\model\\experiments\\exp2\\mask\\regular_mask'
-smaple_dir = 'E:\\model\\experiments\\exp3\\mask\\128'
+smaple_dir = 'E:\\model\\experiments\\exp3\\psv\\mask\\128'
 
 
 def generate_regular_mask(mask_size=256, hole_size=128):
@@ -20,6 +20,6 @@ def generate_regular_mask(mask_size=256, hole_size=128):
 
 
 if __name__ == '__main__':
-    for i in range(1000):
+    for i in range(1490):
         mask = generate_regular_mask()
         imwrite(os.path.join(smaple_dir, 'regular_mask_%04d.png' % (i + 1)), mask)
