e51b3654232c63f06db08509b5be2cad5f839310
fengjiran <fengjiran@foxmail.com>
2019-07-24 13:18:24 +0800

    rename files

--- Inpainting/PIC-EC/rename_files.py
+++ Inpainting/PIC-EC/rename_files.py
@@ -1,6 +1,6 @@
 import os
 
-path = 'E:\\model\\experiments\\exp2\\psv\\gt_images'
+path = 'E:\\model\\experiments\\exp3\\psv\\results\\FMM\\128'
 # path = 'E:\\model\\experiments\\exp2\\mask\\irregular_mask'
 
 file_list = os.listdir(path)
@@ -8,9 +8,9 @@ total_file = len(file_list)
 
 i = 1
 for item in file_list:
-    if item.endswith('.JPG'):
+    if item.endswith('.png'):
         src = os.path.join(os.path.abspath(path), item)
-        dst = os.path.join(os.path.abspath(path), 'gt_img_%03d' % i + '.jpg')
+        dst = os.path.join(os.path.abspath(path), 'test_img_%04d_fake' % i + '.png')
         # dst = os.path.join(os.path.abspath(path), 'irregular_mask_%03d' % i + '.png')
         os.rename(src, dst)
         i += 1
