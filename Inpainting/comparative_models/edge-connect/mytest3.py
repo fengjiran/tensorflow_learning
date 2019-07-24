556ea6f16412297bd1a44a58fa356bce6b64253d
fengjiran <fengjiran@foxmail.com>
2019-07-24 13:19:17 +0800

    test

--- Inpainting/comparative_models/edge-connect/mytest3.py
+++ Inpainting/comparative_models/edge-connect/mytest3.py
@@ -10,21 +10,21 @@ from src.edge_connect import EdgeConnect
 
 
 if __name__ == '__main__':
-    # model_path = 'E:\\model\\comparative_models\\edge_connect\\psv'
-    model_path = 'E:\\model\\comparative_models\\edge_connect\\celeba'
+    model_path = 'E:\\model\\comparative_models\\edge_connect\\psv'
+    # model_path = 'E:\\model\\comparative_models\\edge_connect\\celeba'
 
-    # img_dir = 'E:\\model\\experiments\\exp2\\psv\\gt_images'
-    img_dir = 'E:\\model\\experiments\\exp3\\celebahq\\gt_images'
+    img_dir = 'E:\\model\\experiments\\exp3\\psv\\gt_images'
+    # img_dir = 'E:\\model\\experiments\\exp3\\celebahq\\gt_images'
 
     # Mask or Mask folder
-    regular_mask_dir = 'E:\\model\\experiments\\exp3\\mask\\60'
+    regular_mask_dir = 'E:\\model\\experiments\\exp3\\psv\\mask\\128'
     # irregular_mask_dir = 'E:\\model\\experiments\\exp2\\mask\\irregular_mask'
 
     # Output dir
     # regular_output_dir = 'E:\\model\\experiments\\exp2\\psv\\results\\edge-connect\\regular'
     # irregular_output_dir = 'E:\\model\\experiments\\exp2\\psv\\results\\edge-connect\\irregular'
 
-    regular_output_dir = 'E:\\model\\experiments\\exp3\\celebahq\\results\\edge-connect\\60'
+    regular_output_dir = 'E:\\model\\experiments\\exp3\\psv\\results\\edge-connect\\128'
     # irregular_output_dir = 'E:\\model\\experiments\\exp2\\celebahq\\results\\edge-connect\\irregular'
 
     # 1: edge model, 2: inpaint model, 3: edge-inpaint model, 4: joint model
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(e) for e in config.GPU)
 
     # init device
-    config.DEVICE = torch.device("cpu")
+    config.DEVICE = torch.device("cuda")
 
     # set cv2 running threads to 1 (prevents deadlocks with pytorch dataloader)
     cv2.setNumThreads(0)
