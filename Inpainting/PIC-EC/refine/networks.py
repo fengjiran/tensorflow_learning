8f627123e8d204c87d6caf9e271c04ab1f9704ed
fengjiran <fengjiran@foxmail.com>
2019-07-31 15:41:01 +0800

    network

--- Inpainting/PIC-EC/refine/networks.py
+++ Inpainting/PIC-EC/refine/networks.py
@@ -72,7 +72,9 @@ class RefineNet():
 
             x = conv(x, channels=1, kernel=7, stride=1, pad=3,
                      pad_type='reflect', init_type=self.init_type, name='conv6')
-            edge = tf.nn.sigmoid(x)
+            # edge = tf.nn.sigmoid(x)
+            edge = tf.nn.tanh(x)
+            edge = (edge + 1) / 2.
 
             return edge, x
 
